7667f5bf969db6f897fddb5bfbf4faf1de913348
sun_cs <sun_cs@vanxsoft.net>
2024-06-14 14:32:25 +0800

    递归

---
+++ src/main/java/temp/Temp002.java
@@ -0,0 +1,63 @@
+package temp;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 递归
+ */
+public class Temp002 {
+
+    public static void demoMain1() {
+        int arg = 1;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            System.out.println();
+            int result;
+            try {
+                arg = arg << 1;
+                result = f(arg);
+                System.out.println("when arg is " + arg + ", result is " + result);
+            } catch (StackOverflowError ignored) {
+                System.err.println("stack over flow while arg is " + arg);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        demoMain1();
+        // demoMain2();
+    }
+
+    private static void demoMain2() {
+        int[] cache = new int[1024];
+        Random r = new Random(System.currentTimeMillis());
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i: " + i);
+            System.out.println("cache[" + i + "]: " + cache[i]);
+            System.out.println("---------------------------------------------");
+        }
+    }
+
+    public static Map<Integer, Integer> cache = new HashMap<>();
+    static{
+        cache.put(1, 1);
+        cache.put(2, 2);
+    }
+
+    public static int f(int n) {
+        Integer cacheResult = cache.get(n);
+        if(null != cacheResult){
+            return cacheResult;
+        }
+
+        int result =  f(n - 1) + f(n - 2);
+
+        cache.put(n, result);
+
+        return result;
+    }
+}
