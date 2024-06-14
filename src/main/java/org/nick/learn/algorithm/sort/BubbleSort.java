d78db72dc05e8809420fdd104b7fb8bb0b70df7e
sun_cs <sun_cs@vanxsoft.net>
2024-06-14 15:41:12 +0800

    冒泡排序 - 味对了

--- src/main/java/org/nick/learn/algorithm/sort/BubbleSort.java
+++ src/main/java/org/nick/learn/algorithm/sort/BubbleSort.java
@@ -8,60 +8,26 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static int[] bubbleSort(int[] array) {
-
-        // 脑子没想清楚瞎写的第一版
-//        for (int j = 0; j < array.length; j++) {
-//            for (int i = j; i < array.length - 1; i++) {
-//                int l = array[i];
-//                int r = array[i + 1];
-//                if (l > r) {
-//                    array[i] = r;
-//                    array[i + 1] = l;
-//                }
-//            }
-//        }
-
-        // 脑子想了一想的第一版
-//        for(int indexLast = array.length - 1; indexLast <= 1; indexLast -- ){
-//            for(int i = 0; i < indexLast; i++){
-//                int left = array[i];
-//                int right = array[i + 1];
-//                if(left > right){
-//                    array[i] = right;
-//                    array[i + 1] = left;
-//                }
-//            }
-//        }
-
-        // 脑子想了一想的第二版
-//        for (int indexLast = array.length - 1; indexLast >= 0; indexLast--) {
-//            for (int i = 0; i <= indexLast; i++) {
-//                int left = array[i];
-//                int right = array[i + 1];
-//                if (left > right) {
-//                    array[i] = right;
-//                    array[i + 1] = left;
-//                }
-//            }
-//        }
-
-        // 第二版报错了，改成第三版
         for (int indexLast = array.length - 1; indexLast > 0; indexLast--) {
+            boolean exchangeHappened = false;
             for (int i = 0; i < indexLast; i++) {
                 if (array[i] > array[i + 1]) {
                     int temp = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
+                    exchangeHappened = true;
                 }
             }
+            if(!exchangeHappened){
+                break;
+            }
         }
-
         return array;
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            int[] array = ArrayUtils.generateIntArray(10);
+        for (int i = 0; i < 50; i++) {
+            int[] array = ArrayUtils.generateIntArray(50);
             System.out.println("array: " + Arrays.toString(array));
             array = bubbleSort(array);
             System.out.println("array: " + Arrays.toString(array));
