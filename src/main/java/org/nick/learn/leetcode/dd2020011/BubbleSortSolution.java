2317f5b73b234bfc488a56478a39a29205f96cbb
sun_cs <sun_cs@vanxsoft.net>
2024-11-18 17:35:14 +0800

    冒泡排序

--- src/main/java/org/nick/learn/leetcode/dd2020011/BubbleSortSolution.java
+++ src/main/java/org/nick/learn/leetcode/dd2020011/BubbleSortSolution.java
@@ -1,13 +1,6 @@
 package org.nick.learn.leetcode.dd2020011;
 
-import org.nick.learn.algorithm.common.ArrayUtils;
-
-import java.util.Arrays;
-
 public class BubbleSortSolution {
-    private BubbleSortSolution() {
-
-    }
 
     public static int[] bubbleSort(int[] arr) {
         for (int endIdx = arr.length - 1; endIdx > 0; endIdx--) {
@@ -22,19 +15,5 @@ public class BubbleSortSolution {
         return arr;
     }
 
-    public static void main(String[] args) {
-        int[] arr = ArrayUtils.generateIntArray(10);
 
-        System.out.println("before sort: ");
-        System.out.println(Arrays.toString(arr));
-        System.out.println();
-
-        bubbleSort(arr);
-
-        System.out.println("after sort: ");
-        System.out.println(Arrays.toString(arr));
-        System.out.println();
-
-        System.out.println("arr is asc? " + ArrayUtils.arrayIsAsc(arr));
-    }
 }
---
+++ src/test/java/org/nick/learn/leetcode/dd2020011/BubbleSortSolutionTest.java
@@ -0,0 +1,32 @@
+package org.nick.learn.leetcode.dd2020011;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.nick.learn.algorithm.common.ArrayUtils;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+public class BubbleSortSolutionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void bubbleSort() {
+        for (int i = 0; i < 10; i++) {
+            int[] arr = ArrayUtils.generateIntArray(10000);
+            System.out.println(Arrays.toString(arr));
+            BubbleSortSolution.bubbleSort(arr);
+            assertTrue(ArrayUtils.arrayIsAsc(arr));
+            System.out.println();
+        }
+    }
+}
