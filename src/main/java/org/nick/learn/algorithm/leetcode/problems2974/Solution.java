7ba87b4fed8f628ecc461196d5cf4cb36f2753b2
sun_cs <sun_cs@vanxsoft.net>
2024-12-18 17:21:20 +0800

    快排

---
+++ src/main/java/org/nick/learn/algorithm/leetcode/problems2974/Solution.java
@@ -0,0 +1,21 @@
+package org.nick.learn.algorithm.leetcode.problems2974;
+
+import java.util.Arrays;
+
+/**
+ * 第一版，但是内存浪费了，其实可以原地出组装结果
+ */
+public class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                arr[i + 1] = nums[i];
+            } else {
+                arr[i - 1] = nums[i];
+            }
+        }
+        return arr;
+    }
+}
