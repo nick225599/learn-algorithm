139aaa88a176f8b48f971683e21abf3fb98959e5
sun_cs <sun_cs@vanxsoft.net>
2024-12-25 18:48:48 +0800

    稳定山算法

--- src/main/java/org/nick/learn/leetcode/problems3285/SolutionA.java
+++ src/main/java/org/nick/learn/leetcode/problems3285/SolutionA.java
@@ -9,24 +9,44 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-public class Solution {
+public class SolutionA {
 
-    // 最直观的方案
+    // 贪心，分治，回溯，动态规划
     public List<Integer> stableMountains(int[] height, int threshold) {
-        List<Integer> result = new ArrayList<>(height.length);
-        for (int i = 0; i < height.length - 1;) {
-            if (height[i] > threshold) {
-                result.add(++i);
-            }else{
-                i++;
+        try {
+            ExecutorService es = Executors.newCachedThreadPool();
+            List<Future<Integer>> futures = new ArrayList<>();
+            for (int i = 1; i < height.length; i++) {
+                final int tempI = i;
+                Future<Integer> feature = es.submit(() -> {
+                    if (height[tempI - 1] > threshold) {
+                        return tempI;
+                    } else {
+                        return null;
+                    }
+                });
+                futures.add(feature);
             }
+            es.shutdown();
+            es.awaitTermination(3, TimeUnit.MINUTES);
+            List<Integer> resultList = new ArrayList<>();
+            Integer result;
+            for (Future<Integer> f : futures) {
+                result = f.get(100, TimeUnit.MILLISECONDS);
+                if (null != result) {
+                    resultList.add(result);
+                }
+            }
+
+            return resultList;
+        } catch (Exception ignored) {
+            return null;
         }
-        return result;
     }
 
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        SolutionA solution = new SolutionA();
         int[] mountains;
         List<Integer> indicesOfStableMountains;
 
