339f953cd0bd0cdabb243117fb8afd67f08e2887
sun_cs <sun_cs@vanxsoft.net>
2024-12-27 13:24:57 +0800

    力扣427 构建四叉树

---
+++ src/main/java/org/nick/learn/leetcode/problems427/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 427. Construct Quad Tree
+ * Given a n * n matrix grid of 0's and 1's only. We want to represent grid with a Quad-Tree.
+ * Return the root of the Quad-Tree representing grid.
+ *
+ * @see <a href="https://leetcode.cn/problems/construct-quad-tree/description/"> leetcode 427. Construct Quad Tree</a>
+ */
+package org.nick.learn.leetcode.problems427;
