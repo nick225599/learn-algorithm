35a7e6cda9bb6a1c1fe394e5dcf112c3e752a265
sun_cs <sun_cs@vanxsoft.net>
2024-12-31 14:39:33 +0800

    0-1 背包回溯算法记忆化优化

--- src/main/java/org/nick/learn/algorithm/dynamicprogramming/Knapsack03.java
+++ src/main/java/org/nick/learn/algorithm/dynamicprogramming/Knapsack03.java
@@ -5,17 +5,24 @@ public class Knapsack03 {
     private int[] weight = {2, 2, 4, 6, 3};  // 物品重量
     private int n = 5; // 物品个数
     private int w = 9; // 背包承受的最大重量
-    private boolean[][] memo = new boolean[n][w + 1]; // 记忆化数组
+    private int[][] memo; // 记忆化数组
+
+    public Knapsack03() {
+        memo = new int[n][w + 1];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+    }
 
     public void f(int i, int cw) { // 调用f(0, 0)
         if (cw == w || i == n) { // cw==w表示装满了，i==n表示物品都考察完了
             if (cw > maxW) maxW = cw;
             return;
         }
-        if (memo[i][cw]) { // 如果已经计算过，直接跳过
+        if (memo[i][cw] != -1) { // 如果已经计算过，直接跳过
             return;
-        } else {
-            memo[i][cw] = true; // 存储当前状态的结果
         }
 
         f(i + 1, cw); // 选择不装第i个物品
@@ -23,7 +30,7 @@ public class Knapsack03 {
             f(i + 1, cw + weight[i]); // 选择装第i个物品
         }
 
-
+        memo[i][cw] = maxW; // 存储当前状态的结果
     }
 
     public static void main(String[] args) {
