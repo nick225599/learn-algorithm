20f8b569f6bd2bef0e4432375afbab71538fc672
sun_cs <sun_cs@vanxsoft.net>
2025-01-14 17:00:12 +0800

    排序后穷举，并提前结束，依旧超时

---
+++ src/main/java/org/nick/learn/leetcode/problem15/SolutionC.java
@@ -0,0 +1,40 @@
+package org.nick.learn.leetcode.problem15;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SolutionC {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<String> tmpSet = new TreeSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                // 使用 nums[j]
+                int target = -nums[i] - nums[j];
+                for (int k = nums.length - 1; k > j; k--) {
+                    if (target == nums[k]) {
+                        int[] tmp = new int[3];
+                        tmp[0] = nums[i];
+                        tmp[1] = nums[j];
+                        tmp[2] = nums[k];
+                        Arrays.sort(tmp);
+                        tmpSet.add(tmp[0] + "," + tmp[1] + "," + tmp[2]);
+                    } else if (nums[k] < target) {
+                        break;
+                    }
+                }
+            }
+        }
+        List<List<Integer>> output = new ArrayList<>();
+        for (String str : tmpSet) {
+            List<Integer> list = Arrays.stream(str.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(Collectors.toList());
+            output.add(list);
+
+        }
+        return output;
+    }
+}
