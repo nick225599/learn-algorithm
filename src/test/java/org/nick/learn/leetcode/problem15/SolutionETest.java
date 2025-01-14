2898ec9258b15b43723a327be1f2dd24e1288843
sun_cs <sun_cs@vanxsoft.net>
2025-01-14 17:55:59 +0800

    双指针法

---
+++ src/test/java/org/nick/learn/leetcode/problem15/SolutionETest.java
@@ -0,0 +1,54 @@
+package org.nick.learn.leetcode.problem15;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionETest {
+    private static final SolutionE SOLUTION = new SolutionE();
+
+    @Test
+    void case1() {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> output = SOLUTION.threeSum(nums);
+        assertEquals(2, output.size());
+        for (List<Integer> l : output) {
+            l.sort(Integer::compareTo);
+        }
+        output.sort((o1, o2) -> {
+            for (int i = 0; i < o1.size(); i++) {
+                int compareToResult = o1.get(i).compareTo(o2.get(i));
+                if (0 != compareToResult) {
+                    return compareToResult;
+                }
+            }
+            return 0;
+        });
+        StringBuilder sb = new StringBuilder();
+        for (List<Integer> l : output) {
+            sb.append(Arrays.toString(l.toArray())).append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        assertEquals("[-1, -1, 2], [-1, 0, 1]", sb.toString());
+    }
+
+    @Test
+    void case2() {
+        int[] nums = new int[]{0, 1, 1};
+        List<List<Integer>> output = SOLUTION.threeSum(nums);
+        assertEquals(0, output.size());
+    }
+
+    @Test
+    void case3() {
+        int[] nums = new int[]{0, 0, 0};
+        List<List<Integer>> output = SOLUTION.threeSum(nums);
+        assertEquals(1, output.size());
+        String str = Arrays.toString(output.getFirst().stream().mapToInt(Integer::intValue).toArray());
+        assertEquals("[0, 0, 0]", str);
+    }
+
+}
