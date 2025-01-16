15375ac576341abfc76315a706c7e4c2d71feb0b
sun_cs <sun_cs@vanxsoft.net>
2025-01-16 14:06:10 +0800

    Boyer-Moore投票算法

--- src/main/java/org/nick/learn/leetcode/problem169/SolutionA.java
+++ src/main/java/org/nick/learn/leetcode/problem169/SolutionA.java
@@ -6,8 +6,6 @@ public class SolutionA {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         return nums[nums.length / 2];
-        //TODO sunchuansheng 怎么实现堆排序？
-        //TODO sunchuansheng 随机法(2次尝试) + 分治法
         //TODO sunchuansheng Boyer-Moore投票算法 算法
     }
 }
