8084e450d2a1ee9605047da4ab321bf5982e8ff4
nick225599 <nick225599@163.com>
2025-01-17 18:03:55 +0800

    力扣第 23 题，尝试分治思想

---
+++ src/main/java/org/nick/learn/leetcode/problem23/SolutionC.java
@@ -0,0 +1,18 @@
+package org.nick.learn.leetcode.problem23;
+
+import lombok.extern.slf4j.Slf4j;
+import org.nick.learn.algorithm.list.ListNode;
+
+import java.util.PriorityQueue;
+
+/**
+ * 力扣第 23 题
+ */
+@Slf4j
+public class SolutionC {
+    public ListNode mergeKLists(ListNode[] lists) {
+        //TODO nick_sun 20250117 使用分治思想解决问题
+        //TODO nick_sun 20250117 是否可以使用循环来解决分治产生的栈帧空间使用，降低空间使用率？
+        return null;
+    }
+}
