ad0b20cedf517c319aaf8fb33ade4d0aca85990d
nick225599 <nick225599@163.com>
2025-02-11 16:36:57 +0800

    力扣第 20 题，有效的括号，直觉判断出的解法

---
+++ src/main/java/org/nick/learn/leetcode/problem20/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 力扣第 20 题，有效的括号
+ *
+ * https://leetcode.cn/problems/valid-parentheses/description/
+ */
+package org.nick.learn.leetcode.problem20;
---
+++ src/test/java/org/nick/learn/leetcode/problem20/Solution1Test.java
@@ -0,0 +1,14 @@
+package org.nick.learn.leetcode.problem20;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Solution1Test {
+    Solution1 solution = new Solution1();
+    @Test
+    public void test() {
+        String str = "()";
+        assertTrue(solution.isValid(str));
+    }
+}
