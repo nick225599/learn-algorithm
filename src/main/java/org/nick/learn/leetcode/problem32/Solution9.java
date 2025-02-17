ec75c4655d62185a78b5d039ba9cfdd2cc59a4ef
nick225599 <nick225599@163.com>
2025-02-17 18:42:28 +0800

    力扣第 20 题，有效的括号，完成双端解法

---
+++ src/main/java/org/nick/learn/leetcode/problem32/Solution9.java
@@ -0,0 +1,42 @@
+package org.nick.learn.leetcode.problem32;
+
+public class Solution9 implements Solution {
+
+    // 双端遍历法
+    public int longestValidParentheses(String s) {
+        int leftParenCount = 0;
+        int rightParenCount = 0;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ('(' == c) {
+                leftParenCount++;
+            }
+            if (')' == c) {
+                rightParenCount++;
+                if (leftParenCount == rightParenCount) {
+                    result = Math.max(result, leftParenCount + rightParenCount);
+                } else if (leftParenCount < rightParenCount) {
+                    leftParenCount = rightParenCount = 0;
+                }
+            }
+        }
+        leftParenCount = rightParenCount = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (')' == c) {
+                rightParenCount++;
+            }
+            if ('(' == c) {
+                leftParenCount++;
+                if (leftParenCount == rightParenCount) {
+                    result = Math.max(result, leftParenCount + rightParenCount);
+                } else if (leftParenCount > rightParenCount) {
+                    leftParenCount = rightParenCount = 0;
+                }
+            }
+        }
+        return result;
+    }
+
+}
