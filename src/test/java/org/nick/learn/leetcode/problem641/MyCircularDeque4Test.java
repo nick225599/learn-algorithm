40420c589c7eb8ee7f1c4b81970e00d4153f25c0
nick225599 <nick225599@163.com>
2025-02-19 15:20:36 +0800

    力扣第 641 题，设计循环双端队列，按照自己的想法将两个下标的逻辑统一

---
+++ src/test/java/org/nick/learn/leetcode/problem641/MyCircularDeque4Test.java
@@ -0,0 +1,52 @@
+package org.nick.learn.leetcode.problem641;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyCircularDeque4Test {
+    MyCircularDeque4 deque;
+
+    @Test
+    public void test1() {
+        deque = new MyCircularDeque4(3);    // 设置容量大小为3
+        assertTrue(deque.insertLast(1));                    // 返回 true
+        assertTrue(deque.insertLast(2));                    // 返回 true
+        assertTrue(deque.insertFront(3));                   // 返回 true
+        assertFalse(deque.insertFront(4));                  // 已经满了，返回 false
+        assertEquals(2, deque.getRear());                // 返回 2
+        assertTrue(deque.isFull());                               // 返回 true
+        assertTrue(deque.deleteLast());                           // 返回 true
+        assertTrue(deque.insertFront(4));                   // 返回 true
+        assertEquals(4, deque.getFront());               // 返回 4
+    }
+
+    @Test
+    public void test2() {
+        deque = new MyCircularDeque4(3);    // 设置容量大小为3
+        assertTrue(deque.insertLast(1));                    // 返回 true
+        assertTrue(deque.insertLast(2));                    // 返回 true
+        assertEquals(1, deque.getFront());
+        assertEquals(2, deque.getRear());
+        assertFalse(deque.isFull());
+        assertTrue(deque.deleteLast());;
+        assertTrue(deque.insertFront(4));;
+        assertEquals(4, deque.getFront());
+    }
+
+    @Test
+    public void test() {
+        deque = new MyCircularDeque4( 4);
+        assertTrue(deque.insertFront(9));
+        assertTrue(deque.deleteLast());
+        assertEquals(-1, deque.getRear()); // -1 ????
+        assertEquals(-1, deque.getFront());;
+        assertEquals(-1, deque.getFront());;
+        assertFalse(deque.deleteFront());
+        assertTrue(deque.insertFront(6));
+        assertTrue(deque.insertLast(5));
+        assertTrue(deque.insertFront(9));
+        assertEquals(9, deque.getFront());
+        assertTrue(deque.insertFront(6));
+    }
+}
