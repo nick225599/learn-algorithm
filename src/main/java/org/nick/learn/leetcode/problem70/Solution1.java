606a01c31ee149dd56bbb8a0a970a0bdd5e61f1c
nick225599 <nick225599@163.com>
2025-02-21 16:53:44 +0800

    力扣 70. 爬楼梯，超快的迭代

--- src/main/java/org/nick/learn/leetcode/problem70/Solution1.java
+++ src/main/java/org/nick/learn/leetcode/problem70/Solution1.java
@@ -2,7 +2,7 @@ package org.nick.learn.leetcode.problem70;
 
 public class Solution1 {
 
-    // 递归
+    // 力扣第 70 题
     public int climbStairs(int n) {
         if (n == 1) {
             return 1;
