954638fe3eeab24d021a9236a37492b0dd6e928a
nick225599 <nick225599@163.com>
2025-02-24 17:51:29 +0800

    力扣 70. 爬楼梯，代码简洁

--- src/main/java/org/nick/learn/leetcode/problem70/Solution1.java
+++ src/main/java/org/nick/learn/leetcode/problem70/Solution1.java
@@ -3,22 +3,33 @@ package org.nick.learn.leetcode.problem70;
 public class Solution1 {
 
     // 递归 超时了
+//    public int climbStairs(int n) {
+//        if (n == 1) {
+//            return 1;
+//        }
+//        if (n == 2) {
+//            return 2;
+//        }
+//        int n1 = 0;
+//        if (n - 1 > 0) {
+//            n1 = climbStairs(n - 1);
+//        }
+//        int n2 = 0;
+//        if (n - 2 > 0) {
+//            n2 = climbStairs(n - 2);
+//        }
+//        return n1 + n2;
+//    }
+
+    // 代码可以简洁一些
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        if (n < 1) {
+            throw new IllegalArgumentException();
         }
-        if (n == 2) {
-            return 2;
+        if (n == 1 || n == 2) {
+            return n;
         }
-        int n1 = 0;
-        if (n - 1 > 0) {
-            n1 = climbStairs(n - 1);
-        }
-        int n2 = 0;
-        if (n - 2 > 0) {
-            n2 = climbStairs(n - 2);
-        }
-        return n1 + n2;
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 
 
