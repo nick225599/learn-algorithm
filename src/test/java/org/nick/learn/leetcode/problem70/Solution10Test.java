ad9f65fc3176833c5a61b1e9a36e42d7e70d3fb9
nick225599 <nick225599@163.com>
2025-03-04 18:47:23 +0800

    力扣 70. 爬楼梯，矩阵快速幂解法，不太能理解怎么算齐次线性递推关系式的通项公式

---
+++ src/test/java/org/nick/learn/leetcode/problem70/Solution10Test.java
@@ -0,0 +1,26 @@
+package org.nick.learn.leetcode.problem70;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution10Test {
+
+    Solution10 solution = new Solution10();
+
+    @Test
+    void testClimbStairs() {
+        assertEquals(1, solution.climbStairs(1));
+        assertEquals(2, solution.climbStairs(2));
+        assertEquals(3, solution.climbStairs(3));
+        assertEquals(5, solution.climbStairs(4));
+        assertEquals(8, solution.climbStairs(5));
+        assertEquals(13, solution.climbStairs(6));
+        assertEquals(21, solution.climbStairs(7));
+        assertEquals(34, solution.climbStairs(8));
+        assertEquals(55, solution.climbStairs(9));
+        assertEquals(89, solution.climbStairs(10));
+
+    }
+
+}
