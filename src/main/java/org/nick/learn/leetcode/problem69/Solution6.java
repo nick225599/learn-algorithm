8f064b99717464c4e1c3f5255382eda27a1f1369
nick225599 <nick225599@163.com>
2025-03-05 18:03:03 +0800

    力扣 69. x 的平方根，官方题解二分法 递归版本

---
+++ src/main/java/org/nick/learn/leetcode/problem69/Solution6.java
@@ -0,0 +1,26 @@
+package org.nick.learn.leetcode.problem69;
+
+public class Solution6 {
+
+    // 试位法（二分法）
+    // 尝试将官方题解改成递归
+    public int mySqrt(int x) {
+        return mySqrt(x, 0, x);
+    }
+
+    private int mySqrt(int x, int l, int r) {
+        if (l > r) {
+            return -1;
+        }
+        int mid = l + ((r - l) >> 1);
+        long tmp = (long) mid * mid;
+        if (tmp <= x) {
+            int nextResult = mySqrt(x, mid + 1, r);
+            return Math.max(mid, nextResult);
+        } else {
+            return mySqrt(x, l, mid - 1);
+        }
+    }
+
+
+}
