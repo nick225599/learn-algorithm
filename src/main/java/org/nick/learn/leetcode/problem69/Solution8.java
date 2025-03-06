08f1fc1c08aa441878be2db729d596bd9afdabb3
nick225599 <nick225599@163.com>
2025-03-06 18:07:17 +0800

    力扣 69. x 的平方根，牛顿迭代法

---
+++ src/main/java/org/nick/learn/leetcode/problem69/Solution8.java
@@ -0,0 +1,50 @@
+package org.nick.learn.leetcode.problem69;
+
+public class Solution8 {
+
+    // 牛顿迭代法
+    // X(k+1) = 1/n * ( (n-1) * X(k) + a / ( (X(k)) ^ n ) )
+    // n = 2 时
+    // X(k+1) = 1/2 * (   X(k) + a /( X(K) * X(K) )   )
+
+    // 0 <= x <= 231 - 1
+    public int mySqrt(int x) {
+        if(x == 0){
+            return 0;
+        }
+
+        double epsilon = 1e-10;
+        System.out.println("epsilon: " + epsilon);
+
+        double curNum = x;
+        double nextNum;
+        while(true){
+            nextNum = getNextNum(x, curNum);
+            double delta = Math.abs(nextNum - curNum);
+
+            if(delta <= epsilon){
+                break;
+            }
+
+            curNum = nextNum;
+        }
+
+        System.out.println("curNum: " + curNum);
+
+        //TODO nick 怎么对近似值进行处理，拿到相近值？ 2.999999......9 要入成3
+        // 而 2.8 要舍成 2？
+        return (int) curNum;
+
+
+    }
+
+    /**
+     * TODO 怎么避免浮点数运算的偏差影响结果？
+     *
+     * X(k+1) = 1/2 * (   X(k) + a /( X(K) * X(K) )   )
+     */
+    public double getNextNum(long a, double curNum){
+        return 0.5 * (curNum + a/(curNum * curNum));
+    }
+
+}
