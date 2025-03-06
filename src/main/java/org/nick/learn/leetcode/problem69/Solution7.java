2bf6eac9451814314bc20186ee1e81cfc1c97f0a
nick225599 <nick225599@163.com>
2025-03-06 14:09:40 +0800

    力扣 69. x 的平方根，官方题解计算法没有修正值过大的情况。因为实际上不会出现值过大？

--- src/main/java/org/nick/learn/leetcode/problem69/Solution7.java
+++ src/main/java/org/nick/learn/leetcode/problem69/Solution7.java
@@ -7,7 +7,9 @@ public class Solution7 {
         double tmp = Math.exp(0.5 * Math.log(x));
         int result = (int) tmp;
 
-        //TODO nick 20250305 为啥力扣第 69 题的这个官方题解，不用修正值过大的情况？
+        // Q: nick 20250305 为啥力扣第 69 题的这个官方题解，不用修正值过大的情况？
+        // A: 原则上浮点数运算结果是不准确的，下述修正方式也仅是在题解范围内的 x 有效，生产环境中不适宜直接进行这样的浮点运算
+        // 除非换用 BigDecimal 以及基于 BigDecimal 的 exp() 和 log() 方法
 
         // 修正值过大
          if ((long) result * result > x) {
