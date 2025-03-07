9c8584ec6780801383b8ffb6f25e56b8511a0f96
nick225599 <nick225599@163.com>
2025-03-07 14:32:30 +0800

    力扣 69. x 的平方根，逐位算法

--- src/main/java/org/nick/learn/leetcode/problem69/Solution10.java
+++ src/main/java/org/nick/learn/leetcode/problem69/Solution10.java
@@ -1,9 +1,29 @@
 package org.nick.learn.leetcode.problem69;
 
+import org.nick.learn.algorithm.utils.IntegerUtils;
+
 public class Solution10 {
+
+    // 位运算法
+    public int mySqrt(int x) {
+        int mask = 0x80000000;
+        int result = 0;
+        while (mask != 0) {
+            int tmpResult = result ^ mask;
+            long tmpResultSqrt = (long) tmpResult * tmpResult;
+            if (tmpResultSqrt < x) {
+                result = tmpResult;
+            } else if (tmpResultSqrt == x) {
+                return tmpResult;
+            }
+            mask >>>= 1;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        int x = 24;
-        System.out.println("x: " + String.format("%032b", x));
+        int x = 25;
+        System.out.println("sqrt of " + x + ": " + new Solution10().mySqrt(x));
     }
 
 }
---
+++ src/test/java/org/nick/learn/leetcode/problem69/Solution10Test.java
@@ -0,0 +1,86 @@
+package org.nick.learn.leetcode.problem69;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution10Test {
+
+    Solution10 solution = new Solution10();
+
+    @Test
+    void test1() {
+        int i = 0;
+
+        assertEquals(1, solution.mySqrt(1));
+        System.out.println("ok" + ++i);
+
+        assertEquals(2, solution.mySqrt(4));
+        System.out.println("ok" + ++i);
+
+        assertEquals(3, solution.mySqrt(9));
+        System.out.println("ok" + ++i);
+
+        assertEquals(4, solution.mySqrt(16));
+        System.out.println("ok" + ++i);
+
+        assertEquals(5, solution.mySqrt(25));
+        System.out.println("ok" + ++i);
+
+        assertEquals(6, solution.mySqrt(36));
+        System.out.println("ok" + ++i);
+
+        assertEquals(7, solution.mySqrt(49));
+        System.out.println("ok" + ++i);
+
+        assertEquals(8, solution.mySqrt(64));
+        System.out.println("ok" + ++i);
+
+        assertEquals(9, solution.mySqrt(81));
+        System.out.println("ok" + ++i);
+
+        assertEquals(10, solution.mySqrt(100));
+        System.out.println("ok" + ++i);
+
+        assertEquals(11, solution.mySqrt(121));
+        System.out.println("ok" + ++i);
+
+        assertEquals(12, solution.mySqrt(144));
+        System.out.println("ok" + ++i);
+
+        assertEquals(13, solution.mySqrt(169));
+        System.out.println("ok" + ++i);
+    }
+
+    @Test
+    void test2() throws ExecutionException, InterruptedException {
+        // 二分法，绝对不会出现精度误差
+        Solution5 solution5 = new Solution5();
+
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 1000000; i++) {
+            int finalI = i;
+            Future<Integer> f5 = es.submit(() -> solution5.mySqrt(finalI));
+            Future<Integer> f7 = es.submit(() -> solution.mySqrt(finalI));
+            Integer r5 = f5.get();
+            Integer r7 = f7.get();
+
+            assertEquals(r5, r7);
+
+            if (null != r5 && !r5.equals(r7)) {
+                System.out.println("error! i: " + i + ", r5: " + r5 + ", r7: " + r7);
+            }
+
+            if (i % 10000 == 0) {
+                System.out.println("ok, i: " + i);
+            }
+        }
+        es.shutdown();
+    }
+
+}
