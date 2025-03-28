22bc77bbaa61bb3970d7e50d3ce049b24644b47f
nick225599 <nick225599@163.com>
2025-03-28 13:58:42 +0800

    力扣 69. x 的平方根，手算开平方方法

---
+++ src/main/java/org/nick/learn/leetcode/problem69/Solution14.java
@@ -0,0 +1,40 @@
+package org.nick.learn.leetcode.problem69;
+
+public class Solution14 {
+    public static void main(String[] args) {
+        int n = 25;
+        int sqrt = new Solution14().mySqrt(n);
+        System.out.println("Binary sqrt: " + Integer.toBinaryString(sqrt));
+        System.out.println("Decimal sqrt: " + sqrt);
+    }
+
+    /**
+     * 整方根函数
+     * 手算开平方方法（二进制）
+     */
+    public int mySqrt(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        if (n == 0) return 0;
+
+        int result = 0;
+        int bit = 1 << 30;
+
+        while (bit > n) {
+            bit >>= 2;
+        }
+        System.out.println("Binary bit: " + Integer.toBinaryString(bit));
+        System.out.println("Decimal bit: " + bit);
+
+        while (bit != 0) {
+            int temp = result | bit;
+            if (n >= temp) {
+                n -= temp;
+                result = (result >> 1) | bit;
+            } else {
+                result >>= 1;
+            }
+            bit >>= 2;
+        }
+        return result;
+    }
+}
