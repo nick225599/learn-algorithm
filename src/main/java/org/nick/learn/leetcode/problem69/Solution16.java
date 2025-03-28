8048ecc8858c2940d33de288e49f395ec19b8552
nick225599 <nick225599@163.com>
2025-03-28 16:48:08 +0800

    力扣 69. x 的平方根，手算开平方方法（二进制，并尝试简化）

--- src/main/java/org/nick/learn/leetcode/problem69/Solution16.java
+++ src/main/java/org/nick/learn/leetcode/problem69/Solution16.java
@@ -11,8 +11,8 @@ public class Solution16 {
     }
 
     /**
-     * 整方根函数
-     * 手算开平方方法（二进制） 并尝试简化
+     * 计算整方根函数
+     * 采用手算开平方方法（二进制）
      */
     public int mySqrt(int x) {
         if (x < 0) {
@@ -22,27 +22,28 @@ public class Solution16 {
             return x;
         }
 
-        int pei = 1 << 30; // 两位一组分组，每一组的结束位置
+        int pei; // 两位一组分组，每一组的结束位置
 
         // 计算出第 1 组的结束位置
+        pei = 0b0100_0000__0000_0000__0000_0000__0000_0000;
         while (pei > x) {
             pei >>= 2;
         }
 
-        int result = 0;
-        int p = x;
+        int p = x; // 剩余被开方数
+        int dm = 0;
         while (pei > 0) {
-            int tmp = result | pei;
-            if (p >= tmp) {
+            int tmp = dm | pei;
+            if (tmp <= p) {
+                dm = (dm >> 1) | pei;
                 p = p - tmp;
-                result = (result >> 1) | pei;
             } else {
-                result = result >> 1;
+                dm = dm >> 1;
             }
 
             pei = pei >> 2; // 两两一组，准备开始评估下一组
         }
 
-        return result;
+        return dm;
     }
 }
