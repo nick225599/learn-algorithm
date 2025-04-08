7ea0d0d4ff170c1903874f849a27f1e1b82d514b
nick225599 <nick225599@163.com>
2025-04-08 14:58:57 +0800

    力扣 151. 反转字符串中的单词，遍历反转

---
+++ src/test/java/org/nick/learn/leetcode/problem151/Solution1Test.java
@@ -0,0 +1,17 @@
+package org.nick.learn.leetcode.problem151;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution1Test {
+    Solution1 solution = new Solution1();
+
+    @Test
+    void reverseWords() {
+        String str1 = "hello world";
+        String str2 =  solution.reverseWords(str1);
+        assertEquals("world hello", str2);
+        System.out.println("测试通过");
+    }
+}
