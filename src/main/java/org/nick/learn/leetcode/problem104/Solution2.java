1e4ada94d7c924763814b9d7403bfae6b81dba89
nick225599 <nick225599@163.com>
2025-04-09 14:24:09 +0800

    力扣 104. 二叉树的最大深度，递归统计

--- src/main/java/org/nick/learn/leetcode/problem104/Solution2.java
+++ src/main/java/org/nick/learn/leetcode/problem104/Solution2.java
@@ -6,6 +6,7 @@ public class Solution2 {
 
     /**
      * 基于 org.nick.learn.leetcode.problem104.Solution1 简化代码
+     * 时间复杂度 空间复杂度 依然都是 O(n)
      * @see Solution1
      */
     public int maxDepth(TreeNode root) {
