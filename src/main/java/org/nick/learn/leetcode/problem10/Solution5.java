295c2bf54122eac269f672a138a02297f073b95f
nick225599 <nick225599@163.com>
2025-04-18 14:28:45 +0800

    力扣 10. 正则表达式匹配。尝试从 i=0 j=0 开始迭代

--- src/main/java/org/nick/learn/leetcode/problem10/Solution5.java
+++ src/main/java/org/nick/learn/leetcode/problem10/Solution5.java
@@ -1,7 +1,8 @@
 package org.nick.learn.leetcode.problem10;
 
-public class Solution2 {
+public class Solution5 {
 
+    // 尝试从 i=0 j=0 开始迭代
     public boolean isMatch(String s, String p) {
         int m = s.length();
         int n = p.length();
@@ -15,7 +16,21 @@ public class Solution2 {
         dp[0][0] = 1;
 
         for (int i = 0; i <= s.length(); i++) {
-            for (int j = 1; j <= p.length(); j++) {
+            for (int j = 0; j <= p.length(); j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        dp[i][j] = 1;
+                    } else if (j > 2) {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = 0;
+                    }
+                    continue;
+                }
+                if (j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
                 if (p.charAt(j - 1) != '*') {
                     if (match(s, p, i, j)) {
                         dp[i][j] = dp[i - 1][j - 1];
@@ -35,9 +50,8 @@ public class Solution2 {
     }
 
     private boolean match(String s, String p, int i, int j) {
-        if (i == 0) {
-            return false;
-        }
         return '.' == p.charAt(j - 1) || s.charAt(i - 1) == p.charAt(j - 1);
     }
+
+
 }
