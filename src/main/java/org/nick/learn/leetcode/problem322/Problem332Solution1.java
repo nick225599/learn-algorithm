20480d28829114f731f217b9417a05e9c8e64fa8
nick225599 <nick225599@163.com>
2025-04-22 14:00:57 +0800

    力扣 322. 零钱兑换。回溯递归写法。

--- src/main/java/org/nick/learn/leetcode/problem322/Problem332Solution1.java
+++ src/main/java/org/nick/learn/leetcode/problem322/Problem332Solution1.java
@@ -5,27 +5,36 @@ import java.util.Arrays;
 public class Problem332Solution1 {
 
     private int minCount = -1;
+    private int callCount = 0;
+    private long[] cache;
 
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
-        coinChange(coins, amount, 0, 0);
+
+        // 初始化
+        cache = new long[amount + 1];
+        Arrays.fill(cache, -2);
+
+        coinChange(coins, amount, 0, 0, coins.length - 1);
+        System.out.println("callCount: " + callCount);
         return minCount;
     }
 
-    private void coinChange(int[] coins, int amount, int sum, int count) {
-        if (sum > amount || (minCount != -1 && count > minCount)) {
+    private void coinChange(int[] coins, int amount, int sum, int count, int start) {
+        if (sum == amount) {
+            minCount = count;
             return;
         }
-        if (sum == amount) {
-            if (minCount == -1) {
-                minCount = count;
-            } else {
-                minCount = Math.min(count, minCount);
-            }
+
+        if (cache[sum] != -2 && cache[sum] <= count) {
+            return;
         }
-        for (int i = coins.length - 1; i >= 0; i--) {
-            if (sum <= amount - coins[i]) {
-                coinChange(coins, amount, sum + coins[i], count + 1);
+        cache[sum] = count;
+
+        for (int i = start; i >= 0; i--) {
+            if (sum <= amount - coins[i] && (minCount == -1 || minCount > count)) {
+                coinChange(coins, amount, sum + coins[i], count + 1, i);
+                callCount++;
             }
         }
     }
