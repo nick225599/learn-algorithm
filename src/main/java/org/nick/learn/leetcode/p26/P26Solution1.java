c0f31989be22d57359fedc7c2dd694dbf4eac176
nick225599 <nick225599@163.com>
2025-04-25 09:52:07 +0800

    力扣 26. 删除有序数组中的重复项

--- src/main/java/org/nick/learn/leetcode/p26/P26Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p26/P26Solution1.java
@@ -1,17 +1,38 @@
 package org.nick.learn.leetcode.p26;
 
+import java.util.Arrays;
+
+/**
+ * 力扣 26. 删除有序数组中的重复项
+ */
 public class P26Solution1 {
     public int removeDuplicates(int[] nums) {
-        if(nums.length <= 1){
+        if (nums.length <= 1) {
             return nums.length;
         }
 
-        int fast = 1, slow = 0;
-        for(; fast < nums.length; fast++){
-            if(nums[slow] != nums[fast]){
-                nums[slow++] = nums[fast];
+        int slow = 0;
+        for (int fast = 1; fast < nums.length; fast++) {
+            // 如果当前值 == 慢指针值
+            // 当前值++
+
+            // 如果当前值 ！= 慢指针值
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                if(slow < fast){
+                    nums[slow] = nums[fast];
+                }
             }
+            // 将当前值换至慢指针下一位
+            // 慢指针++
+            // 当前值++
         }
-        return slow;
+        return slow + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 2};
+        System.out.println(new P26Solution1().removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
     }
 }
