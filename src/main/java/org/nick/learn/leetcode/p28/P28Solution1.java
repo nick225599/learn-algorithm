a8b2f6a5fc460c89e4c70038a75fc2956b68dfc0
nick225599 <nick225599@163.com>
2025-04-25 10:57:34 +0800

    力扣 80. 删除有序数组中的重复项 II，三指针，加点注释还是乱乱的，就这样吧能跑就行

--- src/main/java/org/nick/learn/leetcode/p28/P28Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p28/P28Solution1.java
@@ -9,17 +9,24 @@ public class P28Solution1 {
         int rs = 0;
         int re = 0;
         for (int i = 1; i < n; i++) {
+            // 1. 元素重复
+            //    1.1 重复次数 < 2
+            //    1.2 重复次数 >= 2
+            // 2. 元素不重复
             if (nums[i] == nums[re]) {
-                if (rs == re) {
+                // 重复元素且重复次数 < 2，将重复元素复制到下一位
+                if (re == rs) {
                     nums[++re] = nums[i];
+                } else {
+                    // 重复次数 >= 2 时，直接无视，继续遍历下一位
                 }
             } else {
-                ++re;
-                rs = re;
+                // 非重复元素，则将其复制到下一位
                 if (re < i) {
-                    nums[re] = nums[i];
+                    nums[++re] = nums[i];
                 }
-
+                // 然后重置重复去间
+                rs = re;
             }
         }
         return re + 1;
