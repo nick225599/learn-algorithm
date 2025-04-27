0f47e8468c365f5f06621912a2e9b81c891a9e22
nick225599 <nick225599@163.com>
2025-04-27 09:49:28 +0800

    力扣 189. 轮转数组，待学习时间空间复杂度为 O(1) 的解决方案

--- src/main/java/org/nick/learn/leetcode/p189/P189Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p189/P189Solution3.java
@@ -3,20 +3,20 @@ package org.nick.learn.leetcode.p189;
 /**
  * 力扣 189. 轮转数组
  */
-public class P189Solution2 {
+public class P189Solution3 {
 
     /**
      * 时间复杂度 O(n)
      * 空间复杂度 O(n)
      *
      * 还能有空间复杂度更低的解法吗？
+     *
+     * 有的，用固定长度的临时空间来缓存要被覆盖的元素
      */
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        int[] newNums = new int[n];
-        for (int i = 0; i < n; i++) {
-            newNums[(i + k) % n] = nums[i];
-        }
-        System.arraycopy(newNums, 0, nums, 0, n);
+
     }
+
+
+
 }
