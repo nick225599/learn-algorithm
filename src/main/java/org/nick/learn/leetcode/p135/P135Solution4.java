c7f8c1010155a6da3d3c3cac5621aa9e9795ebb3
nick225599 <nick225599@163.com>
2025-05-08 17:40:16 +0800

    力扣 P135，一次遍历法，咋还报错

--- src/main/java/org/nick/learn/leetcode/p135/P135Solution4.java
+++ src/main/java/org/nick/learn/leetcode/p135/P135Solution4.java
@@ -4,50 +4,30 @@ public class P135Solution4 {
 
     /**
      * 一次遍历法
-     *
-     * //TODO nick 20250508 有报错，看下官方题解
      */
     public int candy(int[] ratings) {
         int n = ratings.length;
 
-        int sum = 1;
+        int ret = 1;
+        int asc = 1;
+        int dec = 0;
         int pre = 1;
-        int ascLength = 1;
-        int descLength = 0;
         for (int i = 1; i < n; i++) {
-            if (ratings[i - 1] < ratings[i]) {
-                int cur = pre + 1;
-                sum += cur;
-                ascLength++;
-                descLength = 0;
-                pre = cur;
-            } else if (ratings[i - 1] == ratings[i]) {
-                int cur = 1;
-                sum += cur;
-                ascLength = 1;
-                descLength = 0;
+            if (ratings[i - 1] <= ratings[i]) {
+                int cur = (ratings[i - 1] == ratings[i]) ? 1 : pre + 1;
+                ret += cur;
+                asc++;
                 pre = cur;
             } else {
-                int cur = pre - 1;
-                if (cur >= 1) {
-                    sum += cur;
-                    descLength += 1;
-                    pre = cur;
-                } else {
-                    cur = 1;
-                    sum += cur;
-                    sum += descLength;
-                    descLength++;
-                    if(ascLength == descLength){
-                        sum +=1;
-                        ascLength--;
-                        descLength++;
-                    }
+                dec++;
+                if(dec == asc){
+                    dec++;
                 }
+                ret += dec;
+                pre = 1;
             }
         }
 
-
-        return sum;
+        return ret;
     }
 }
