0e6eb012b0136e178fed9126b5eb1efd191b4e25
nick225599 <nick225599@163.com>
2025-05-08 17:45:03 +0800

    力扣 P135，一次遍历法，写对了，官方题解真巧妙

--- src/main/java/org/nick/learn/leetcode/p135/P135Solution4.java
+++ src/main/java/org/nick/learn/leetcode/p135/P135Solution4.java
@@ -8,26 +8,29 @@ public class P135Solution4 {
     public int candy(int[] ratings) {
         int n = ratings.length;
 
-        int ret = 1;
+        int sum = 1;
         int asc = 1;
-        int dec = 0;
+        int desc = 0;
         int pre = 1;
         for (int i = 1; i < n; i++) {
             if (ratings[i - 1] <= ratings[i]) {
+                desc = 0;
                 int cur = (ratings[i - 1] == ratings[i]) ? 1 : pre + 1;
-                ret += cur;
-                asc++;
+                sum += cur;
                 pre = cur;
+//                asc++; // asc 不用 == 0？
+                asc = cur; // 需要的，直接用 cur 的值赋值一下就行
             } else {
-                dec++;
-                if(dec == asc){
-                    dec++;
+                desc++;
+                if(desc == asc){
+                    desc++; // asc 不用 -- ？
+                    // 不用在 if(){} 中能够被正确地赋值为 1
                 }
-                ret += dec;
+                sum += desc;
                 pre = 1;
             }
         }
 
-        return ret;
+        return sum;
     }
 }
