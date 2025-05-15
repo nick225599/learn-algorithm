bfdb992f6ca3b201540cdd181f96318baa4b8a21
nick225599 <nick225599@163.com>
2025-05-15 16:40:19 +0800

    力扣 28. BM 算法

--- src/main/java/org/nick/learn/leetcode/p28/P28Solution4.java
+++ src/main/java/org/nick/learn/leetcode/p28/P28Solution4.java
@@ -1,14 +1,44 @@
 package org.nick.learn.leetcode.p28;
 
+import java.util.Arrays;
+
 public class P28Solution4 {
 
     /**
-     * KMP 解法
-     * KMP 算法是根据三位作者（D.E.Knuth，J.H.Morris 和 V.R.Pratt）的名字来命名的，
-     * 算法的全称是 Knuth Morris Pratt 算法，简称为 KMP 算法。
+     * BM 算法
      */
     public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
 
+        if (n < m) return -1;
+
+        int[] badChar = new int[123];
+        Arrays.fill(badChar, -1);
+        for (int i = 0; i < m; i++) {
+            badChar[needle.charAt(i)] = i;
+        }
+
+        int i = 0;
+        while (i <= n - m) {
+            int j = m - 1;
+            while (j >= 0 && haystack.charAt(i + j) == needle.charAt(j)) {
+                j--;
+            }
+            if (j < 0) {
+                return i;
+            }
+
+            // aaabaaa
+            // baaa
+            //    baaa
+
+            // 偏移量 = 失配符号在模式串中对应的位置 - 失配符号在模式串中的位置
+            int shift = j - badChar[haystack.charAt(i + j)];
+
+            shift = Math.max(1, shift);
+            i += shift;
+        }
         return -1;
     }
 
