5de48566fc45422de09bb660bbc2b05707f926e1
nick225599 <nick225599@163.com>
2025-05-22 10:57:22 +0800

    力扣 125. 验证回文串，搞半天题解说用 API...

--- src/main/java/org/nick/learn/leetcode/p125/P125Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p125/P125Solution1.java
@@ -3,38 +3,55 @@ package org.nick.learn.leetcode.p125;
 public class P125Solution1 {
     public boolean isPalindrome(String s) {
         int n = s.length();
-        int i = 0, j = 0;
-        while (i + j < s.length() - 2) {
-            int intI = 0;
-            while (intI < 'a' || intI > 'z') {
-                intI = s.charAt(i);
-                if ('A' <= intI && intI <= 'Z') {
-                    intI = intI + ('a' - 'Z');
+        int i = 0, j = n - 1;
+        while (i < j) {
+            char charI;
+            do {
+                if (i >= n) {
+                    charI = '&';
+                    break;
                 }
-                i++;
-            }
-            int intJ = 0;
-            while (intJ < 'a' || intJ > 'z') {
-                intJ = s.charAt(n - 1 - j);
-                if ('A' <= intJ && intJ <= 'Z') {
-                    intJ = intJ + ('a' - 'Z');
+                charI = s.charAt(i++);
+                charI = this.toLowerCase(charI);
+            } while (charI < '0' || (charI > '9' && charI < 'a') || charI > 'z');
+
+            char charJ;
+            do {
+                if (j < 0) {
+                    charJ = '&';
+                    break;
                 }
-                j++;
-            }
-            if (intI != intJ) {
+                charJ = s.charAt(j--);
+                charJ = this.toLowerCase(charJ);
+            } while (charJ < '0' || (charJ > '9' && charJ < 'a') || charJ > 'z');
+            if (charI != charJ) {
                 return false;
             }
 
+            // 过半直接终止循环
+            if (i > j) {
+                return true;
+            }
         }
         return true;
     }
 
+    private char toLowerCase(char charI) {
+        if (charI >= 'A' && charI <= 'Z') {
+            charI = (char) (charI + ('a' - 'A'));
+        }
+        return charI;
+    }
+
     public static void main(String[] args) {
+
         System.out.println((int) 'a'); // 97
         System.out.println((int) 'z'); // 122
         System.out.println((int) 'A'); // 65
         System.out.println((int) 'Z'); // 90
-        System.out.println(122 - 97);
-        System.out.println(90 - 65);
+        System.out.println((int) '0'); // 48
+        System.out.println((int) '1'); // 49
+        System.out.println((int) '2'); // 50
+        System.out.println((int) '9'); // 57
     }
 }
--- src/test/java/org/nick/learn/leetcode/p125/P125Solution1Test.java
+++ src/test/java/org/nick/learn/leetcode/p125/P125Solution1Test.java
@@ -2,6 +2,7 @@ package org.nick.learn.leetcode.p125;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class P125Solution1Test {
@@ -13,5 +14,30 @@ class P125Solution1Test {
         String str = "A man, a plan, a canal: Panama";
         boolean b = solution.isPalindrome(str);
         assertTrue(b);
+    }
+    @Test
+    void isPalindrome2() {
+        String str = "ab";
+        boolean b = solution.isPalindrome(str);
+        assertFalse(b);
+    }
+    @Test
+    void isPalindrome3() {
+        String str = ".,";
+        boolean b = solution.isPalindrome(str);
+        assertTrue(b);
+    }
+    @Test
+    void isPalindrome4() {
+        String str = "a.";
+        boolean b = solution.isPalindrome(str);
+        assertTrue(b);
+    }
+
+    @Test
+    void isPalindrome5() {
+        String str = "0P";
+        boolean b = solution.isPalindrome(str);
+        assertFalse(b);
     }
 }
