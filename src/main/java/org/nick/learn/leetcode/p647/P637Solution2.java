e6c3182236a3bd1693dad8b1a012ab7ca2904771
nick225599 <nick225599@163.com>
2025-06-04 15:04:53 +0800

    637. 二叉树的层平均值，一次成功

---
+++ src/main/java/org/nick/learn/leetcode/p647/P637Solution2.java
@@ -0,0 +1,40 @@
+package org.nick.learn.leetcode.p647;
+
+import org.nick.learn.leetcode.p100.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class P637Solution2 {
+
+    // 官方题解，巧用 queue.size() 一个方法解决问题
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> averages = new ArrayList<Double>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            double sum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                TreeNode left = node.left, right = node.right;
+                if (left != null) {
+                    queue.offer(left);
+                }
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+            averages.add(sum / size);
+        }
+        return averages;
+    }
+
+//    作者：力扣官方题解
+//    链接：https://leetcode.cn/problems/average-of-levels-in-binary-tree/solutions/410522/er-cha-shu-de-ceng-ping-jun-zhi-by-leetcode-soluti/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
