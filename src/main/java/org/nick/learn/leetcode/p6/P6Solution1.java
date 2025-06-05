fef260ff00f8bad9a6af2ff4eac37c6f2f030be2
nick225599 <nick225599@163.com>
2025-06-05 16:49:22 +0800

    力扣 6. Z 字形变换，自己写通了，代码有点冗杂

--- src/main/java/org/nick/learn/leetcode/p6/P6Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p6/P6Solution1.java
@@ -3,14 +3,14 @@ package org.nick.learn.leetcode.p6;
 public class P6Solution1 {
     public String convert(String s, int numRows) {
         int n = s.length();
-        if (n <= numRows) {
+        if (n <= numRows || numRows == 1) {
             return s;
         }
 
         StringBuilder sb = new StringBuilder();
-        int tmp = 0;
         for (int i = 0; i < numRows; i++) {
             sb.append(s.charAt(i));
+            int tmp = 0;
             for (int j = i; j < n; ) {
                 tmp ^= 1;
                 int diff;
--- src/test/java/org/nick/learn/leetcode/p6/P6Solution1Test.java
+++ src/test/java/org/nick/learn/leetcode/p6/P6Solution1Test.java
@@ -25,4 +25,21 @@ class P6Solution1Test {
         assertEquals("PAHNAPLSIIGYIR", solution.convert("PAYPALISHIRING", 3));
     }
 
+    // PAYPALISHIRING, 4
+    // P     I     N
+    // A   L S   I G
+    // Y A   H R
+    // P     I
+    // PIN ALSIG YAHR PI
+    // PIN APSIG YAHR PI [ERROR]
+    @Test
+    public void test3(){
+        assertEquals("PINALSIGYAHRPI", solution.convert("PAYPALISHIRING", 4));
+    }
+
+    @Test
+    public void test4(){
+        assertEquals("AB", solution.convert("AB", 1));
+    }
+
 }
