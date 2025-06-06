b21e26058eed3ffccfbdf6071002a66c2e03bae7
nick225599 <nick225599@163.com>
2025-06-06 14:46:37 +0800

    力扣 3. 无重复字符的最长子串，调试好了

--- src/main/java/org/nick/learn/leetcode/p3/P3Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p3/P3Solution1.java
@@ -5,27 +5,55 @@ import java.util.Set;
 
 public class P3Solution1 {
     public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
         int n = s.length();
 
-        Set<Character> charSet = new HashSet<>();
-        int i = 0;
-        int j = 0;
-        int length = 0;
-        int maxLength = 0;
-        while (j < n) {
-            boolean b = charSet.add(s.charAt(j));
-            if (b) {
-                length++;
-                maxLength = Math.max(maxLength, length);
+        int left = 0;
+        int right = 0;
+        int maxLength = 1;
+        while (right < n) {
+            char c = s.charAt(right);
+            boolean addSuccess = charSet.add(c);
+            if (addSuccess) {
+                maxLength = Math.max(maxLength, right - left + 1);
+                right++;
             } else {
-                do {
-                    charSet.remove(s.charAt(i));
-                    length--;
-                    i++;
-                } while (s.charAt(i - 1) != s.charAt(j));
+                while (!charSet.remove(s.charAt(left++))) {}
             }
-            j++;
         }
         return maxLength;
+
+//        int n = s.length();
+//
+//        Set<Character> charSet = new HashSet<>();
+//        int i = 0;
+//        charSet.add(s.charAt(i));
+//        int j = 1;
+//        int length = 1;
+//        int maxLength = 1;
+//        while (j < n) {
+//            boolean b = charSet.add(s.charAt(j));
+//            if (b) {
+//                length++;
+//                maxLength = Math.max(maxLength, length);
+//                j++;
+//            } else {
+//                while(s.charAt(i) != s.charAt(j)){
+//                    charSet.remove(s.charAt(i));
+//                    i++;
+//                    length--;
+//                }
+//                charSet.remove(s.charAt(i));
+//                i++;
+//                length--;
+////                do {
+////                    charSet.remove(s.charAt(i));
+////                    length--;
+////                    i++;
+////                } while (s.charAt(i - 1) != s.charAt(j));
+//            }
+//
+//        }
+//        return maxLength;
     }
 }
--- src/test/java/org/nick/learn/leetcode/p3/P3Solution1Test.java
+++ src/test/java/org/nick/learn/leetcode/p3/P3Solution1Test.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class P3Solution1Test {
+    P3Solution1 solution = new P3Solution1();
 
     @Test
     void lengthOfLongestSubstring() {
+        assertEquals(2, solution.lengthOfLongestSubstring("aab"));
     }
 }
