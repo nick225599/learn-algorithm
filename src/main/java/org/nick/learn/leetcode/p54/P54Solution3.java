a41735b0385200f6f65305642a9cc6c6c4937598
nick225599 <nick225599@163.com>
2025-06-06 17:30:12 +0800

    力扣 54. 螺旋矩阵，学着官方题解的样子简化了一下代码

--- src/main/java/org/nick/learn/leetcode/p54/P54Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p54/P54Solution3.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class P54Solution3 {
     public List<Integer> spiralOrder(int[][] matrix) {
+
         int n = matrix.length;
         int m = matrix[0].length;
+        boolean[][] visited = new boolean[n][m];
         int total = n * m;
         int i = 0;
         int j = 0;
@@ -14,18 +16,31 @@ public class P54Solution3 {
         String direction = "右";
         for (int k = 0; k < total; k++) {
             result.add(matrix[i][j]);
-            matrix[i][j] = Integer.MAX_VALUE;
+            visited[i][j] = true;
+
+            // 根据当前方向，试算出下个元素的位置
             int[] nextIJ = this.nextIJ(direction, i, j);
-            if (nextIJ[0] < 0 || nextIJ[0] >= n
-                    || nextIJ[1] < 0 || nextIJ[1] >= m
-                    || matrix[nextIJ[0]][nextIJ[1]] == Integer.MAX_VALUE) {
+
+            // 根据试算结果，判断是否需要转向
+            boolean atTheBoundary = this.isAtTheBoundary(nextIJ, matrix, visited);
+
+            // 如果需要转向，则转向并重新计算下一个元素的位置
+            if (atTheBoundary) {
                 direction = this.nextDirection(direction);
                 nextIJ = this.nextIJ(direction, i, j);
             }
+
+            // 不会出现需要连续转向的情况，所以直接更新 i, j 即可
             i = nextIJ[0];
             j = nextIJ[1];
         }
         return result;
+    }
+
+    private boolean isAtTheBoundary(int[] nextIJ, int[][] matrix, boolean[][] visited) {
+        return (nextIJ[0] < 0 || nextIJ[0] >= matrix.length)
+                || (nextIJ[1] < 0 || nextIJ[1] >= matrix[0].length)
+                || visited[nextIJ[0]][nextIJ[1]];
     }
 
     private int[] nextIJ(String direction, int i, int j) {
@@ -48,8 +63,4 @@ public class P54Solution3 {
         };
     }
 
-//    作者：力扣官方题解
-//    链接：https://leetcode.cn/problems/spiral-matrix/solutions/275393/luo-xuan-ju-zhen-by-leetcode-solution/
-//    来源：力扣（LeetCode）
-//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 }
