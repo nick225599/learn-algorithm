aadd026262a40056931ad06645945eb8e6330cbd
nick225599 <nick225599@163.com>
2025-06-08 13:21:22 +0800

    力扣 54. 螺旋矩阵，学着官方题解的样子简化了一下代码

---
+++ src/main/java/org/nick/learn/leetcode/p54/P54Solution4.java
@@ -0,0 +1,73 @@
+package org.nick.learn.leetcode.p54;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P54Solution4 {
+    // 上一个题解用  String direction 和 boolean[][] visited ， 是否需要转向
+    // 但其实可以通过计算直接算出来，简化这个 O(nm) 的空间损耗
+    // 一圈圈往里获取元素并写入 List<Integer>
+    // 这也是我第一反应的解法，
+    // 只是不会写成代码，写着写着就自然而言想用标记来替代坐标换算逻辑
+    //TODO nick 尝试将坐标换算逻辑写出来
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+//
+//        int n = matrix.length;
+//        int m = matrix[0].length;
+//        boolean[][] visited = new boolean[n][m];
+//        int total = n * m;
+//        int i = 0;
+//        int j = 0;
+//        List<Integer> result = new ArrayList<>(total);
+//        String direction = "右";
+//        for (int k = 0; k < total; k++) {
+//            result.add(matrix[i][j]);
+//            visited[i][j] = true;
+//
+//            // 根据当前方向，试算出下个元素的位置
+//            int[] nextIJ = this.nextIJ(direction, i, j);
+//
+//            // 根据试算结果，判断是否需要转向
+//            boolean atTheBoundary = this.isAtTheBoundary(nextIJ, matrix, visited);
+//
+//            // 如果需要转向，则转向并重新计算下一个元素的位置
+//            if (atTheBoundary) {
+//                direction = this.nextDirection(direction);
+//                nextIJ = this.nextIJ(direction, i, j);
+//            }
+//
+//            // 不会出现需要连续转向的情况，所以直接更新 i, j 即可
+//            i = nextIJ[0];
+//            j = nextIJ[1];
+//        }
+//        return result;
+    }
+
+//    private boolean isAtTheBoundary(int[] nextIJ, int[][] matrix, boolean[][] visited) {
+//        return (nextIJ[0] < 0 || nextIJ[0] >= matrix.length)
+//                || (nextIJ[1] < 0 || nextIJ[1] >= matrix[0].length)
+//                || visited[nextIJ[0]][nextIJ[1]];
+//    }
+//
+//    private int[] nextIJ(String direction, int i, int j) {
+//        return switch (direction) {
+//            case "上" -> new int[]{i - 1, j};
+//            case "下" -> new int[]{i + 1, j};
+//            case "左" -> new int[]{i, j - 1};
+//            case "右" -> new int[]{i, j + 1};
+//            default -> throw new IllegalArgumentException();
+//        };
+//    }
+//
+//    private String nextDirection(String direction) {
+//        return switch (direction) {
+//            case "上" -> "右";
+//            case "下" -> "左";
+//            case "左" -> "上";
+//            case "右" -> "下";
+//            default -> throw new IllegalArgumentException();
+//        };
+//    }
+
+}
