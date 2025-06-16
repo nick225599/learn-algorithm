946ad1e98380740a955dd161918d2443c7ff5875
nick225599 <nick225599@163.com>
2025-06-16 17:47:16 +0800

    力扣 106. 从中序与后序遍历序列构造二叉树，加注释

--- src/main/java/org/nick/learn/leetcode/p106/P106Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p106/P106Solution2.java
@@ -23,8 +23,17 @@ public class P106Solution2 {
         for (int i = n - 2; i >= 0; i--) {
             TreeNode node = new TreeNode(postorder[i]);
 
-            if (inorder[inorderIndex] == stack.peek().val) {
+            // 判断栈顶节点是不是没有右子树
+            if (inorder[inorderIndex] != stack.peek().val) { // 堆栈顶部的元素没有右子树
+                // 有右子树
+                stack.peek().right = node;
+            } else {
+                // 没有右子树
+
+                // 没有右子树的话，那么当前元素就是栈顶元素或者栈顶元素的某个父元素的左子树
                 TreeNode parent = null;
+
+                // 根据中序和后续的逆序性来跳过没有左节点的栈顶元素
 
                 // 写成这样就错了， while ( inorder[inorderIndex] == stack.peek().val) {
                 while (!stack.isEmpty() && inorder[inorderIndex] == stack.peek().val) {
@@ -32,8 +41,6 @@ public class P106Solution2 {
                     inorderIndex--;
                 }
                 parent.left = node;
-            } else { // 堆栈顶部的元素没有右子树
-                stack.peek().right = node;
             }
             stack.push(node);
         }
