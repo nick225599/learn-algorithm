86b719aae910bfac406fa447997542cf459eeffd
nick225599 <nick225599@163.com>
2025-06-16 17:38:49 +0800

    力扣 106. 从中序与后序遍历序列构造二叉树，遍历怎么写不对了呢

---
+++ src/main/java/org/nick/learn/leetcode/p106/P106Solution2.java
@@ -0,0 +1,41 @@
+package org.nick.learn.leetcode.p106;
+
+import org.nick.learn.leetcode.p100.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+public class P106Solution2 {
+
+    // in-order: L, val, R
+    // post-order: L, R, val
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int n = postorder.length;
+
+        TreeNode root = new TreeNode(postorder[n - 1]);
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        int inorderIndex = n - 1;
+        for(int i = n - 2; i >= 0; i--){
+            TreeNode node = new TreeNode(postorder[i]);
+            if(inorder[inorderIndex] != stack.peek().val){ // 堆栈顶部的元素没有右子树
+                stack.peek().right = node;
+            }else{
+                TreeNode parent = null;
+                while(inorder[inorderIndex] != stack.peek().val){
+                    parent = stack.pop();
+                    inorderIndex--;
+                }
+                parent.left = node;
+            }
+        }
+
+        return root;
+    }
+
+
+}
