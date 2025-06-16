80d577d3be657925b51dbf7cc4ad1c7866aee69d
nick225599 <nick225599@163.com>
2025-06-16 17:44:31 +0800

    力扣 106. 从中序与后序遍历序列构造二叉树，迭代也过了

--- src/main/java/org/nick/learn/leetcode/p106/P106Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p106/P106Solution2.java
@@ -4,8 +4,6 @@ import org.nick.learn.leetcode.p100.TreeNode;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
 
 public class P106Solution2 {
 
@@ -17,20 +15,25 @@ public class P106Solution2 {
         TreeNode root = new TreeNode(postorder[n - 1]);
 
         Deque<TreeNode> stack = new ArrayDeque<>();
+
         stack.push(root);
 
         int inorderIndex = n - 1;
-        for(int i = n - 2; i >= 0; i--){
+
+        for (int i = n - 2; i >= 0; i--) {
             TreeNode node = new TreeNode(postorder[i]);
-            if(inorder[inorderIndex] != stack.peek().val){ // 堆栈顶部的元素没有右子树
-                stack.peek().right = node;
-            }else{
+
+            if (inorder[inorderIndex] == stack.peek().val) {
                 TreeNode parent = null;
-                while(inorder[inorderIndex] == stack.peek().val){
+
+                // 写成这样就错了， while ( inorder[inorderIndex] == stack.peek().val) {
+                while (!stack.isEmpty() && inorder[inorderIndex] == stack.peek().val) {
                     parent = stack.pop();
                     inorderIndex--;
                 }
                 parent.left = node;
+            } else { // 堆栈顶部的元素没有右子树
+                stack.peek().right = node;
             }
             stack.push(node);
         }
