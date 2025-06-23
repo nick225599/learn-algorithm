d62fba5796927c05d45a861cd2e9b1cf2585fea3
nick225599 <nick225599@163.com>
2025-06-23 16:04:31 +0800

    力扣 230. 130. 被围绕的区域，毫无思路，怎么感觉都是 O(n^2) 的解法

--- src/main/java/org/nick/learn/leetcode/p130/P130Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p130/P130Solution1.java
@@ -1,37 +1,27 @@
 package org.nick.learn.leetcode.p130;
 
+import java.util.Calendar;
+
 public class P130Solution1 {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
 
 
-        // 上 从上往下看
-        int upX = 0b1000;
-        int upO = 0b0000;
-        // 下 从下往上看
-        int downX = 0b0100;
-        int downO = 0b0000;
-        // 左 从左往右看
-        int leftX = 0b0010;
-        int leftO = 0b0000;
-        // 右 从右往左看
-        int rightX = 0b0001;
-        int rightO = 0b0000;
-
-        int[][] tmp = new int[n][m];
+        char[][] up = new char[n][m]; // 上边界
+        char[][] down = new char[n][m]; // 下边界
+        char[][] left = new char[n][m]; // 左边界
+        char[][] right = new char[n][m]; // 右边界
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 char c = board[i][j];
 
-                tmp[i][j] = 0;
-
                 if (c == 'X') {
-                    tmp[i][j] = upX | downX | leftX | rightX;
+                    up[i][j] = down[i][j] = left[i][j] = right[i][j] = 'X';
                 }
                 if (c == 'O') {
-                    tmp[i][j] = -1;
-                    //TODO nick 写到这儿
+                    up[i][j] = down[i - 1][j];
+                    left[i][j] = right[i][j - 1];
                 }
 
 
@@ -39,4 +29,5 @@ public class P130Solution1 {
         }
 
     }
+
 }
