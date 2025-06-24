bbf9d5ec2682f41cd9c46e96cc2a2565058ccb1e
nick225599 <nick225599@163.com>
2025-06-24 13:37:32 +0800

    力扣 133. 克隆图，双层缓存没用还是有死循环，明天再看了

--- src/main/java/org/nick/learn/leetcode/p133/P133Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p133/P133Solution1.java
@@ -5,37 +5,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
 public class P133Solution1 {
-    public Node cloneGraph(Node node) {
-        Map<Integer, Node> buff1 = new HashMap<>();
-        Map<Integer, Node> buff2 = new HashMap<>();
 
-        return cloneGraph(node, buff1, buff2);
+    public Node cloneGraph(Node node) {
+
+        // 第一次 copy 只将值复制
+        Map<Integer, Node> buff = new HashMap<>();
+        this.copy1(node, buff);
+
+//        // 第二次 copy 将引用复制
+//        Map<Integer, Node> buff2 = new HashMap<>();
+//        this.copy2(node, buff, buff2);
+//
+//        return buff2.get(node.val);
+        return null;
     }
 
-    private Node cloneGraph(Node node, Map<Integer, Node> buff1, Map<Integer, Node> buff2) {
-        Node newNode = buff2.get(node.val);
-        if(newNode != null){
-            return newNode;
-        }
-
-        newNode = buff1.get(node.val);
+    private void copy1(Node node, Map<Integer, Node> buff) {
+        Node newNode = buff.get(node.val);
         if(newNode == null){
             newNode = new Node(node.val);
-            buff1.put(node.val, newNode);
+            buff.put(node.val, newNode);
         }
-
-        List<Node> list = new ArrayList<>(node.neighbors.size());
-        for (Node n : node.neighbors) {
-            list.add(cloneGraph(n, buff1, buff2));
+        buff.computeIfAbsent(node.val, k->new Node(node.val));
+        for(Node n: node.neighbors){
+            this.copy1(n, buff);
         }
-        newNode.neighbors = list;
-
-        buff1.remove(node.val);
-        buff2.put(node.val, newNode);
-
-        return newNode;
     }
+
+//    private void copy2(Node node, Map<Integer, Node> shallowCpoyMap, Map<Integer, Node> deepCopyMap) {
+//        Node newNode = buff.get(node.val);
+//        List<Node> newNeighbors = new ArrayList<>(node.neighbors.size());
+//        for(Node neighbor : node.neighbors){
+//            newNeighbors.add(buff.get(neighbor.val));
+//        }
+//        newNode.neighbors = newNeighbors;
+//
+//        buff.put(node.val, newNode);
+//        return newNode;
+//    }
+
 }
