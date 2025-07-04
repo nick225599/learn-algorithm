74e1dc7898c329ffbce6e96991968ed145d40193
nick225599 <nick225599@163.com>
2025-07-04 17:38:46 +0800

    学习到 力扣 207 210

---
+++ src/main/java/org/nick/learn/leetcode/p207/P207Solution3.java
@@ -0,0 +1,27 @@
+package org.nick.learn.leetcode.p207;
+
+import java.util.*;
+
+public class P207Solution3 {
+
+    class Node{
+        int val;
+        Node pre;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    // 尝试失败，监测不出两链或三链的环
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//        for(int[] prerequisite: prerequisites){
+//            int
+//        }
+//
+//
+        return false;
+
+    }
+}
---
+++ src/main/java/org/nick/learn/leetcode/p210/P210Solution1.java
@@ -0,0 +1,7 @@
+package org.nick.learn.leetcode.p210;
+
+public class P210Solution1 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        return null;
+    }
+}
