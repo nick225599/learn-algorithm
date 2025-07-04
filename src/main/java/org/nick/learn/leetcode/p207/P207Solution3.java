7d208e402d12f1cb30800f2437e83432085342ad
nick sun <nick.evans@outlook.com>
2025-07-04 21:48:43 +0800

    力扣 207. 课程表，尝试用邻接表 + bfs or dfs

--- src/main/java/org/nick/learn/leetcode/p207/P207Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p207/P207Solution3.java
@@ -1,27 +1,39 @@
 package org.nick.learn.leetcode.p207;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class P207Solution3 {
 
-    class Node{
-        int val;
-        Node pre;
-        Node next;
-
-        public Node(int val) {
-            this.val = val;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] prerequisite : prerequisites) {
+            int cur = prerequisite[0];
+            int prereq = prerequisite[1];
+            List<Integer> ableList = graph.computeIfAbsent(prereq, k -> new ArrayList<>());
+            ableList.add(cur);
         }
+
+        for (Map.Entry<Integer, List<Integer>> entry1 : graph.entrySet()) {
+            Integer key1 = entry1.getKey();
+            List<Integer> values1 = entry1.getValue();
+            // 已知 key1 -> key2
+            for (Integer key2 : values1) {
+                // 在图中找有没有 key2 -> key1 的路径
+                if (this.bfs(graph, key2, key1)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
     }
 
-    // 尝试失败，监测不出两链或三链的环
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-//        for(int[] prerequisite: prerequisites){
-//            int
-//        }
-//
-//
-        return false;
+    private boolean bfs(Map<Integer, List<Integer>> graph, Integer start, Integer end) {
 
+        return false;
     }
 }
