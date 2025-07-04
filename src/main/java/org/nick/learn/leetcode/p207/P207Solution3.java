d56e86f60631512ca94c98be63b03ad1a1c7d19a
nick sun <nick.evans@outlook.com>
2025-07-04 22:12:02 +0800

    力扣 207. 课程表，尝试用邻接表 + 不知道是 bfs 还是 dfs 进行查询

--- src/main/java/org/nick/learn/leetcode/p207/P207Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p207/P207Solution3.java
@@ -1,9 +1,6 @@
 package org.nick.learn.leetcode.p207;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class P207Solution3 {
 
@@ -12,6 +9,7 @@ public class P207Solution3 {
         for (int[] prerequisite : prerequisites) {
             int cur = prerequisite[0];
             int prereq = prerequisite[1];
+            if(cur == prereq){ return false; }
             List<Integer> ableList = graph.computeIfAbsent(prereq, k -> new ArrayList<>());
             ableList.add(cur);
         }
@@ -22,7 +20,7 @@ public class P207Solution3 {
             // 已知 key1 -> key2
             for (Integer key2 : values1) {
                 // 在图中找有没有 key2 -> key1 的路径
-                if (this.bfs(graph, key2, key1)) {
+                if (this.pathExist(graph, key2, key1)) {
                     return false;
                 }
             }
@@ -32,8 +30,75 @@ public class P207Solution3 {
 
     }
 
-    private boolean bfs(Map<Integer, List<Integer>> graph, Integer start, Integer end) {
+    private boolean pathExist(Map<Integer, List<Integer>> graph, Integer start, Integer end) {
+        for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
+            Integer key = entry.getKey();
+            List<Integer> value = entry.getValue();
 
+            if (key.equals(start)) {
+                // 在 value 以及 value 衍生 sub-value 中找 end
+                Queue<List<Integer>> valueQueue = new LinkedList<>();
+                valueQueue.add(value);
+
+                while (true) {
+                    value = valueQueue.poll();
+                    if (value == null) {
+                        return false;
+                    }
+                    if (value.contains(end)) {
+                        return true;
+                    }
+                    for (Integer val : value) {
+                        if (graph.containsKey(val)) {
+                            valueQueue.add(graph.get(val));
+                        }
+                    }
+                }
+
+            } else {
+                // 在 value 以及 value 衍生 sub-value 中找 start
+                // 在 value 以及 value 衍生 sub-value 中找 end
+                Queue<List<Integer>> valueQueue = new LinkedList<>();
+                valueQueue.add(value);
+
+                while (true) {
+                    value = valueQueue.poll();
+                    if (value == null) {
+                        return false;
+                    }
+                    if (value.contains(start)) {
+
+                        // 在 start 对应的 value 及 sub-val 中找 end
+                        valueQueue.clear();
+                        value = graph.get(start);
+                        valueQueue.add(value);
+
+                        while(true){
+                            value = valueQueue.poll();
+                            if (value == null) {
+                                return false;
+                            }
+                            if (value.contains(end)) {
+                                return true;
+                            }
+                            for (Integer val : value) {
+                                if (graph.containsKey(val)) {
+                                    valueQueue.add(graph.get(val));
+                                }
+                            }
+                        }
+
+
+
+                    }
+                    for (Integer val : value) {
+                        if (graph.containsKey(val)) {
+                            valueQueue.add(graph.get(val));
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 }
