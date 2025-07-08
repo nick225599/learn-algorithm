11d641cd4f1c34cc1a8d1c9eb26f5aef642b057e
nick225599 <nick225599@163.com>
2025-07-08 17:49:17 +0800

    leetcode 909. Snakes and Ladders，优化后的版本

--- src/main/java/org/nick/learn/leetcode/p909/P909Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p909/P909Solution2.java
@@ -1,7 +1,6 @@
 package org.nick.learn.leetcode.p909;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class P909Solution2 {
@@ -16,40 +15,41 @@ public class P909Solution2 {
         }
     }
 
-    // 让 AI 优化一下
+    // 让 AI 优化一下，5ms
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
         int total = n * n;
 
+        boolean[] visited = new boolean[total + 1];
+
         Queue<MoveStatus> queue = new LinkedList<>();
         queue.offer(new MoveStatus(1, 0));
+        visited[1] = true;
 
-        boolean[] visited = new boolean[total + 1];
         while (!queue.isEmpty()) {
             MoveStatus cur = queue.poll();
-            visited[cur.cur] = true;
             if (cur.cur == total) {
                 return cur.steps;
-            } else {
-                for(int i = 1; i <= 6; i++){
-                    int next = cur.cur + i;
-                    if(next > total){
-                        break;
-                    }
-                    int[] arr = index2coordinate(n, next);
-                    int x = arr[0];
-                    int y = arr[1];
-                    if(board[x][y] != -1){
-                        next = board[x][y];
-                    }
-                    if(!visited[next]){
-                        queue.add(new MoveStatus(next, cur.steps + 1));
-                    }
+            }
+            for (int i = 1; i <= 6; i++) {
+                int next = cur.cur + i;
+                if (next > total) {
+                    break;
+                }
+                int[] arr = index2coordinate(n, next);
+                int x = arr[0];
+                int y = arr[1];
+                if (board[x][y] != -1) {
+                    next = board[x][y];
+                }
+                if (!visited[next]) {
+                    queue.add(new MoveStatus(next, cur.steps + 1));
+                    visited[next] = true;
                 }
             }
         }
 
-        return  -1;
+        return -1;
     }
 
     private int[] index2coordinate(int widthOfBoard, int index) {
@@ -69,8 +69,13 @@ public class P909Solution2 {
         return new int[]{row, column};
     }
 
-
-
+//    private int[] index2coordinate(int n, int index) {
+//        index--; // 转换为0-based索引
+//        int row = n - 1 - (index / n);
+//        int colBase = index % n;
+//        int col = (n - 1 - row) % 2 == 0 ? colBase : n - 1 - colBase;
+//        return new int[]{row, col};
+//    }
 
 
 }
