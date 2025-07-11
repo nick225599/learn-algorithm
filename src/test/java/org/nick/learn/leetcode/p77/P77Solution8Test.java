0bad7a9f459e5383a5bcd5f2f16475b81d0f2623
nick225599 <nick225599@163.com>
2025-07-11 15:16:52 +0800

    leetcode 77. Combinations，怎么老是 out of memory

--- src/test/java/org/nick/learn/leetcode/p77/P77Solution8Test.java
+++ src/test/java/org/nick/learn/leetcode/p77/P77Solution8Test.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-class P77Solution8Test {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class P77Solution8Test {
     P77Solution8 solution = new P77Solution8();
+
+    P77Solution8 solution8 = new P77Solution8();
+    P77Solution10 solution10 = new P77Solution10();
 
     @Test
     void combine() {
@@ -17,5 +21,25 @@ class P77Solution8Test {
         for (List<Integer> tmp : ans) {
             System.out.println(Arrays.toString(tmp.toArray()));
         }
+    }
+
+    @Test
+    void combine2() {
+        int[] arr = new int[100];
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            List<List<Integer>> ans = solution8.combine(i, 3);
+            arr[i] = ans.size();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("elapse: " + (end - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            List<List<Integer>> ans = solution10.combine(i, 3);
+            assertEquals(arr[i], ans.size());
+        }
+        end = System.currentTimeMillis();
+        System.out.println("elapse: " + (end - start));
     }
 }
