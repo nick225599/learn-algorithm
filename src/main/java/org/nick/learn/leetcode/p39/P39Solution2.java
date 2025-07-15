ce314453e8a638192cb855861554026aa8f8c9ad
nick225599 <nick225599@163.com>
2025-07-15 10:54:08 +0800

    leetcode 39. Combination Sum，官方题解 “搜索回溯” 法

---
+++ src/main/java/org/nick/learn/leetcode/p39/P39Solution2.java
@@ -0,0 +1,41 @@
+package org.nick.learn.leetcode.p39;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P39Solution2 {
+
+    //    作者：力扣官方题解
+    //    链接：https://leetcode.cn/problems/combination-sum/solutions/406516/zu-he-zong-he-by-leetcode-solution/
+    //    来源：力扣（LeetCode）
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+    // 官方题解 “搜索回溯” 法
+    // 2ms 击败 88.99%
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+
+
+}
