69ec02f82fe38e1a019227fbbfff9c698ceeeb91
nick sun <nick.evans@outlook.com>
2025-07-15 22:46:25 +0800

    leetcode 77. Combinations，哪里不对？

--- src/main/java/org/nick/learn/leetcode/p52/P52Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p52/P52Solution2.java
@@ -4,26 +4,21 @@ package org.nick.learn.leetcode.p52;
 public class P52Solution2 {
     int n;
     int result;
-
     int count;
 
-    boolean[] queueRows; // 哪几行存在皇后
-    boolean[] queueColumns; // 哪几列存在皇后
-
-    int row; // 当前在第几行
-    int column; // 当前在第几列
-    boolean[] rowFlag; // 当前行哪几列被皇后影响
-
+    boolean[] forbiddenColumns;
+    boolean[] forbiddenLeft;
+    boolean[] forbiddenRight;
 
     // 785ms 击败6%
     public int totalNQueens(int n) {
+        this.n = n;
         this.result = 0;
         this.count = 0;
-
-        this.n = n;
-
+        forbiddenColumns = new boolean[n];
+        forbiddenLeft = new boolean[n];
+        forbiddenRight = new boolean[n];
         this.dfs(0);
-
         return result;
     }
 
@@ -32,44 +27,33 @@ public class P52Solution2 {
             return;
         }
 
-        int row = index / n;
+        if (count == n) {
+            result++;
+            return;
+        }
+
         int column = index % n;
+
+
+
+        // 放
+        if (!forbiddenColumns[column] && !forbiddenLeft[column] && !forbiddenRight[column]) {
+
+            this.count++;
+            forbiddenColumns[column] = true;
+            forbiddenLeft[(column + n - 1) % n] = true;
+            forbiddenRight[(column + 1) % n] = true;
+
+            this.dfs(index + 1);
+
+            this.count--;
+            forbiddenColumns[column] = false;
+            forbiddenLeft[(column + n - 1) % n] = false;
+            forbiddenRight[(column + 1) % n] = false;
+        }
 
         // 不放
         this.dfs(index + 1);
-
-        // 放
-        // 从上一层的 rowFlag 计算出当前层
-        this.rowFlag = nextFloor(rowFlag);
-        this.count++;
-        queueRows[row] = true;
-        queueColumns[column] = true;
-
-
-        this.dfs(index + 1);
     }
-
-    private boolean[] nextFloor(boolean[] rowFlag) {
-        boolean left = true;
-        for (int i = 0; i < n; i++) {
-            if (rowFlag[i]) {
-                if (left) {
-                    if (i - 1 >= 0) {
-                        rowFlag[i - 1] = true;
-                    }
-                    rowFlag[i] = false;
-                    left = false;
-                } else {
-                    if (i + 1 < n) {
-                        rowFlag[i + 1] = true;
-                    }
-                    rowFlag[i] = false;
-                }
-            }
-
-        }
-        return null;
-    }
-
 
 }
