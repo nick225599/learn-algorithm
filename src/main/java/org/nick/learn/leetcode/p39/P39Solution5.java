4cffec6b3ff6bebc1649e1aabe2bfebebe46d902
nick225599 <nick225599@163.com>
2025-07-15 14:39:58 +0800

    leetcode 39. Combination Sum，优化不了一点

--- src/main/java/org/nick/learn/leetcode/p39/P39Solution5.java
+++ src/main/java/org/nick/learn/leetcode/p39/P39Solution5.java
@@ -6,34 +6,31 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class P39Solution5 {
-    List<List<Integer>> ans;
-    List<Integer> list;
-    int[] candidates;
 
-    // 自己摸索出来的解法，
-    // 3ms 击败 26%
+    // 用 ArrayList 就 1ms
+    // 用 LinkedList 就 2ms
+    // 就挺无语......
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        this.ans = new LinkedList<>();
-        this.list = new LinkedList<>();
-        this.candidates = candidates;
-
-
-        backtrack(target, candidates.length - 1); // 从大到小挑
+        List<List<Integer>> ans = new LinkedList<>();
+        backtrack(ans, new ArrayList<>(), 0, target, candidates, 0);
         return ans;
     }
 
-    private void backtrack(int remain, int start) {
-        if (remain == 0) {
-            ans.add(new ArrayList<>(list));
+    private void backtrack(List<List<Integer>> ans, List<Integer> output, int sum, int target, int[] candidates,
+                           int startIndex) {
+        if(sum == target){
+            ans.add(new ArrayList<>(output));
             return;
         }
 
-        for (int i = start; i >= 0; i--) {
-            if (remain >= candidates[i]) {
-                list.add(candidates[i]);
-                backtrack(remain - candidates[i], i);
-                list.removeLast();
+        for(int i = startIndex; i < candidates.length; i++){
+            int candidate = candidates[i];
+            if(sum + candidate > target){
+                continue;
             }
+            output.add(candidate);
+            backtrack(ans, output, sum + candidate, target, candidates, i);
+            output.removeLast();
         }
     }
 
