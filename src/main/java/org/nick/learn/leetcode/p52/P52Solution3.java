8b728fff69fa538988a7a582ec3074de52b05772
nick225599 <nick225599@163.com>
2025-07-16 10:57:46 +0800

    leetcode 52. N-Queens II，理解一下官方题解

---
+++ src/main/java/org/nick/learn/leetcode/p52/P52Solution3.java
@@ -0,0 +1,49 @@
+package org.nick.learn.leetcode.p52;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P52Solution3 {
+
+    // 作者：力扣官方题解
+    // 链接：https://leetcode.cn/problems/n-queens-ii/solutions/449388/nhuang-hou-ii-by-leetcode-solution/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    public int totalNQueens(int n) {
+        Set<Integer> columns = new HashSet<>();
+        Set<Integer> diagonals1 = new HashSet<>();
+        Set<Integer> diagonals2 = new HashSet<>();
+        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    public int backtrack(int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (columns.contains(i)) {
+                continue;
+            }
+            int diagonal1 = row - i; // 不用担心越界
+            if (diagonals1.contains(diagonal1)) { // 不是判断当前列？
+                continue;
+            }
+            int diagonal2 = row + i;
+            if (diagonals2.contains(diagonal2)) {
+                continue;
+            }
+            columns.add(i);
+            diagonals1.add(diagonal1);
+            diagonals2.add(diagonal2);
+            count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
+            columns.remove(i);
+            diagonals1.remove(diagonal1);
+            diagonals2.remove(diagonal2);
+        }
+        return count;
+    }
+
+
+}
