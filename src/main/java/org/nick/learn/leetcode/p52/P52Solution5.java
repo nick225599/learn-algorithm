2c0096611138dd10c8da1656c5ee5a3b6ab84106
nick225599 <nick225599@163.com>
2025-07-16 14:01:19 +0800

    leetcode 52. N-Queens II，官方题解二，基于位图和位运算的回溯

---
+++ src/main/java/org/nick/learn/leetcode/p52/P52Solution5.java
@@ -0,0 +1,32 @@
+package org.nick.learn.leetcode.p52;
+
+public class P52Solution5 {
+    //     作者：力扣官方题解
+    //    链接：https://leetcode.cn/problems/n-queens-ii/solutions/449388/nhuang-hou-ii-by-leetcode-solution/
+    //    来源：力扣（LeetCode）
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+    // 基于位图和位运算的回溯
+    public int totalNQueens(int n) {
+        return solve(n, 0, 0, 0, 0);
+    }
+
+    public int solve(int n, int row, int columns, int diagonals1, int diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                count += solve(n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+            }
+            return count;
+        }
+    }
+
+
+
+
+}
