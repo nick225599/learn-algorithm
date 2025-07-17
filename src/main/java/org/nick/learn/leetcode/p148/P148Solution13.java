feca1bdb09c50e998ded517068de3c9386fba815
nick225599 <nick225599@163.com>
2025-07-17 10:41:12 +0800

    leetcode 148. Sort List，O(1) 空间复杂度官方题解

--- src/main/java/org/nick/learn/leetcode/p148/P148Solution13.java
+++ src/main/java/org/nick/learn/leetcode/p148/P148Solution13.java
@@ -57,25 +57,21 @@ public class P148Solution13 {
         return dummyHead.next;
     }
 
-    public ListNode merge(ListNode head1, ListNode head2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
-        while (temp1 != null && temp2 != null) {
-            if (temp1.val <= temp2.val) {
-                temp.next = temp1;
-                temp1 = temp1.next;
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode();
+        ListNode node = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                node.next = left;
+                left = left.next;
             } else {
-                temp.next = temp2;
-                temp2 = temp2.next;
+                node.next = right;
+                right = right.next;
             }
-            temp = temp.next;
+            node = node.next;
         }
-        if (temp1 != null) {
-            temp.next = temp1;
-        } else if (temp2 != null) {
-            temp.next = temp2;
-        }
-        return dummyHead.next;
+        node.next = left != null ? left : right;
+        return dummy.next;
     }
 
 
