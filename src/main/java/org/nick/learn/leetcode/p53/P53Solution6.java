0c61528ff6f5e7f5ed66331a8d99cffe0fde11cc
nick225599 <nick225599@163.com>
2025-07-17 17:55:55 +0800

    leetcode 53. Maximum Subarray，分治算法，哪里不对？？？？

--- src/main/java/org/nick/learn/leetcode/p53/P53Solution6.java
+++ src/main/java/org/nick/learn/leetcode/p53/P53Solution6.java
@@ -2,52 +2,41 @@ package org.nick.learn.leetcode.p53;
 
 public class P53Solution6 {
 
-    static class CompoundSum {
-        int lSum;
-        int rSum;
-        int mSum;
-        int iSum;
-
-        public CompoundSum(int lSum, int rSum, int mSum, int iSum) {
-            this.lSum = lSum;
-            this.rSum = rSum;
-            this.mSum = mSum;
-            this.iSum = iSum;
-        }
-    }
-
-    // 分支法
+    // 分治法
     // 有点跟第 148 题链表排序神似
+    // 8ms
+    // 时间复杂度 logn
+    // 空间复杂度 logn
     public int maxSubArray(int[] nums) {
         int n = nums.length;
-        CompoundSum compoundSum = maxSubArray(nums, 0, n - 1);
-        return compoundSum.mSum;
+        int[] compoundSum = maxSubArray(nums, 0, n - 1);
+        return compoundSum[2];
     }
 
     // 找最大值
-    private CompoundSum maxSubArray(int[] nums, int i, int j) {
+    private int[] maxSubArray(int[] nums, int i, int j) {
         if (i == j) {
-            return new CompoundSum(nums[i], nums[i], nums[i], nums[i]);
+            return new int[]{nums[i], nums[i], nums[i], nums[i]};
         }
 
         // 分
         int mid = i + ((j - i) >> 1);
-        CompoundSum l = maxSubArray(nums, i, mid);
-        CompoundSum r = maxSubArray(nums, mid + 1, j);
+        int[] l = maxSubArray(nums, i, mid);
+        int[] r = maxSubArray(nums, mid + 1, j);
 
         // 合
         // lSum 表示 [l,r] 内以 l 为左端点的最大子段和
-        int lSum = l.iSum + r.lSum;
+        int lSum = Math.max(l[0], l[3] + r[0]);
         // rSum 表示 [l,r] 内以 r 为右端点的最大子段和
-        int rSum = l.rSum + r.iSum;
+        int rSum = Math.max(r[1], l[1] + r[3]);
         // mSum 表示 [l,r] 内的最大子段和
         int mSum = Math.max(
-                Math.max(l.mSum, r.mSum),
-                l.rSum + r.lSum
+                Math.max(l[2], r[2]),
+                l[1] + r[0]
         );
         // iSum 表示 [l,r] 的区间和
-        int iSum = l.iSum + r.iSum;
-        return new CompoundSum(lSum, rSum, mSum, iSum);
+        int iSum = l[3] + r[3];
+        return new int[]{lSum, rSum, mSum, iSum};
     }
 
 
--- src/test/java/org/nick/learn/leetcode/p53/P53Solution6Test.java
+++ src/test/java/org/nick/learn/leetcode/p53/P53Solution6Test.java
@@ -2,13 +2,44 @@ package org.nick.learn.leetcode.p53;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class P53Solution6Test {
     P53Solution6 solution = new P53Solution6();
+
     @Test
     void maxSubArray() {
         int a = solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
+        assertEquals(6, a);
+    }
+
+    @Test
+    void maxSubArra1() {
+        int a = solution.maxSubArray(new int[]{1, 2});
+        assertEquals(3, a);
+    }
+
+    @Test
+    void maxSubArra2() {
+        int a = solution.maxSubArray(new int[]{1, -1});
+        assertEquals(1, a);
+    }
+
+    @Test
+    void maxSubArra3() {
+        int a = solution.maxSubArray(new int[]{-1, 1});
+        assertEquals(1, a);
+    }
+
+    @Test
+    void maxSubArra4() {
+        int a = solution.maxSubArray(new int[]{9, -6, 4});
+        assertEquals(9, a);
+    }
+
+    @Test
+    void maxSubArra5() {
+        int a = solution.maxSubArray(new int[]{-9, 6, -4});
         assertEquals(6, a);
     }
 }
