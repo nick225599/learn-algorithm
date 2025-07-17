f2143d458f5161d327649a19fe23b1a767a4cb73
nick225599 <nick225599@163.com>
2025-07-17 10:13:29 +0800

    leetcode 148. Sort List，快慢指针找中间位置

---
+++ src/main/java/org/nick/learn/leetcode/p148/P148Solution12.java
@@ -0,0 +1,21 @@
+package org.nick.learn.leetcode.p148;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class P148Solution12 {
+
+    // Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+    // Nick: O(1) memory (?_ ? )
+    public ListNode sortList(ListNode head) {
+        ListNode holyShit = new ListNode();
+        return holyShit;
+    }
+}
