590d46e60fbcc9237d3d752580a886661e732d4f
nick sun <nick.evans@outlook.com>
2025-07-17 22:49:33 +0800

    leetcode 918. Maximum Sum Circular Subarray，没想好怎么解题

--- src/main/java/org/nick/learn/leetcode/p918/P918Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p918/P918Solution1.java
@@ -2,38 +2,22 @@ package org.nick.learn.leetcode.p918;
 
 public class P918Solution1 {
 
-    // 1ms 击败100%
-    //TODO nick 把这个 dp 状态变化方程捋一捋
+    //TODO nick 20250717 看了下题，O(n) 的平方？
     public int maxSubarraySumCircular(int[] nums) {
-
-        int n = nums.length;
-        int[] newNums = new int[n<<1];
-        System.arraycopy(nums, 0, newNums, 0, n);
-        System.arraycopy(nums, 0, newNums, n, n);
-        return this.maxSubArray(newNums);
-
-    }
-
-    // 看了下官方题解进行进一步优化
-    // 1ms 击败 100%
-    // 这还有个专名的算法名： Kadane 算法，可惜我出生晚了，不然我就有机会让这个算法叫 NICK SUN 算法
-    public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
         int n = nums.length;
 
         int pre = nums[0];
         int max = nums[0];
-        for(int i = 1; i < n; i++){
-            pre = Math.max(nums[i], nums[i] + pre);
-            max = Math.max(max, pre);
+        for (int i = 0; i < (n << 1); i++) {
+            int cur = nums[i % n];
+            pre = Math.max(pre, pre + cur);
+            max = Math.max(pre, max);
         }
-
         return max;
-    }
 
+        // 没想好怎么做，但隐隐觉得 O(n) 就行
+
+    }
 
 
 }
