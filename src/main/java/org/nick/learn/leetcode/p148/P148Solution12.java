b06de8ca2e7bf2e3d8e9fec30896f2a0a4830c28
nick225599 <nick225599@163.com>
2025-07-17 10:37:21 +0800

    leetcode 148. Sort List，快慢指针找中间位置

--- src/main/java/org/nick/learn/leetcode/p148/P148Solution12.java
+++ src/main/java/org/nick/learn/leetcode/p148/P148Solution12.java
@@ -15,6 +15,7 @@ public class P148Solution12 {
     // Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
     // Nick: O(1) memory (?_ ? )
     public ListNode sortList(ListNode head) {
+        // 我特么看了一遍题解思路都没看懂...
         ListNode holyShit = new ListNode();
         return holyShit;
     }
