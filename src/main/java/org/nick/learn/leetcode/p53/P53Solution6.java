be463aad985d0f7a002c65899033c1613574b6be
nick225599 <nick225599@163.com>
2025-07-17 16:57:20 +0800

    leetcode 53. Maximum Subarray，分治算法，哪里不对？？？？

--- src/main/java/org/nick/learn/leetcode/p53/P53Solution6.java
+++ src/main/java/org/nick/learn/leetcode/p53/P53Solution6.java
@@ -2,49 +2,48 @@ package org.nick.learn.leetcode.p53;
 
 public class P53Solution6 {
 
+    static class CompoundSum {
+        int lSum;
+        int rSum;
+        int mSum;
+        int iSum;
+
+        public CompoundSum(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+
     // 分支法
     // 有点跟第 148 题链表排序神似
     public int maxSubArray(int[] nums) {
         int n = nums.length;
-        int[] arr =  maxSubArray(nums, 0, n - 1);
-        return arr[2];
+        CompoundSum compoundSum = maxSubArray(nums, 0, n - 1);
+        return compoundSum.mSum;
     }
 
     // 找最大值
-    private int[] maxSubArray(int[] nums, int i, int j) {
+    private CompoundSum maxSubArray(int[] nums, int i, int j) {
         if (i == j) {
-            // 分无可分
-            return new int[] {nums[i], nums[i], nums[i], nums[i]};
-
-        } else{
-
-            // 分
-            int mid = i + ((j - i) >> 1);
-            int[] left = maxSubArray(nums, i, mid);
-            int[] right = maxSubArray(nums, mid + 1, j);
-
-            // 以 i 为左端点的最大子数组的和
-            // = subL.整个数组的和 + subR.以 i 为左端点的最大子数组的和
-            int i0 = left[3] + right[0];
-
-            // 以 j 为右端点的最大子数组的和
-            // = subL.以 j 为右端点的最大子数组的和 + subR.整个数组的和
-            int i1 = left[1] + right[3];
-
-            // [i, j] 数组中最大子数组的和
-            // = subL.最大和，subR.最大和，subL.以 j 为右端点的最大和 + subR.以 i 为左端点的最大和
-            // 三者取最大值
-            int i2 = Math.max(
-                    Math.max(left[2], right[2]),
-                    left[1] + right[0]
-            );
-
-            // 整个数组的和
-            int i3 = left[3] + right[3];
-
-            // 合并
-            return new int[]{i0, i1, i2, i3};
+            return new CompoundSum(nums[i], nums[i], nums[i], nums[i]);
         }
+
+        // 分
+        int mid = i + ((j - i) >> 1);
+        CompoundSum l = maxSubArray(nums, i, mid);
+        CompoundSum r = maxSubArray(nums, mid + 1, j);
+
+        // 合
+        int lSum = l.iSum + r.lSum;
+        int rSum = l.rSum + r.iSum;
+        int mSum = Math.max(
+                Math.max(l.mSum, r.mSum),
+                l.rSum + r.lSum
+        );
+        int iSum = l.iSum + r.iSum;
+        return new CompoundSum(lSum, rSum, mSum, iSum);
     }
 
 
