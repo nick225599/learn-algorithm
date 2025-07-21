eb5c34806e524fafd19615ca8d7cd7ff9988f97c
nick225599 <nick225599@163.com>
2025-07-21 14:59:13 +0800

    leetcode 918. Maximum Sum Circular Subarray，降低系数

--- src/main/java/org/nick/learn/leetcode/p918/P918Solution5.java
+++ src/main/java/org/nick/learn/leetcode/p918/P918Solution5.java
@@ -9,7 +9,7 @@ public class P918Solution5 {
     // 空间复杂度 O(n)
 
     // 怎么优化下常数系数？
-    // 6ms 击败 69%
+    // 4ms 击败 95%
     public int maxSubarraySumCircular(int[] nums) {
         int n = nums.length;
 
@@ -19,14 +19,16 @@ public class P918Solution5 {
         }
 
 //        int[] arr0 = new int[n]; // 以 i 为下标，表示 nums[0, i] 的元素和
-//        int[] arr1 = new int[n]; // 以 i 为下标，表示 nums[i, n - 1] 的元素和
-//        int[] arr2 = new int[n]; // 以 i 为下标，表示 nums[0, i] 的以 0 为左边界的子数组的最大和
-        int[] arr3 = new int[n]; // 以 i 为下标，表示 nums[i, n - 1] & [0, i - 1] 子数组的最大和
-
         int arr0 = nums[0];
-        int arr1 = sum;
+
+//        int[] arr1 = new int[n]; // 以 i 为下标，表示 nums[i, n - 1] 的元素和
+        int arr1;
+
+//        int[] arr2 = new int[n]; // 以 i 为下标，表示 nums[0, i] 的以 0 为左边界的子数组的最大和
         int arr2 = nums[0];
-        arr3[0] = nums[0];
+
+//        int[] arr3 = new int[n]; // 以 i 为下标，表示 nums[i, n - 1] & [0, i - 1] 子数组的最大和
+        int arr3;
 
         int pre = nums[0];
         int max = nums[0];
@@ -42,12 +44,12 @@ public class P918Solution5 {
 
             // [i,n-1] 的元素和 + [0, i-1]以 0  为左边界的子数组最大和
             // = 跨右边界的子数组的最大和 [i, n - 1] & [0, i - 1]
-            arr3[i] = arr1 + arr2;
+            arr3 = arr1 + arr2;
+
+            finalMax = Math.max(finalMax, arr3);
+
             // [0, i] 的最大前缀和 = [0, i - 1] 最大前缀和 或 [0, i-1]的和 + 当前位
             arr2 = Math.max(arr2, arr0 + nums[i]);
-            finalMax = Math.max(finalMax, arr3[i]);
-
-
             arr0 = arr0 + nums[i];
         }
         finalMax = Math.max(finalMax, max);
