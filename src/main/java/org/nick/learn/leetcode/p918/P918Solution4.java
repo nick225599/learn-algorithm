35696fcc0848c8a3bac72c94545526379a150e84
nick225599 <nick225599@163.com>
2025-07-21 14:17:59 +0800

    leetcode 918. Maximum Sum Circular Subarray，DP法

--- src/main/java/org/nick/learn/leetcode/p918/P918Solution4.java
+++ src/main/java/org/nick/learn/leetcode/p918/P918Solution4.java
@@ -1,5 +1,7 @@
 package org.nick.learn.leetcode.p918;
 
+import java.util.Arrays;
+
 public class P918Solution4 {
 
     // dp
@@ -7,25 +9,46 @@ public class P918Solution4 {
         int n = nums.length;
 
         int[] arr1 = new int[n]; // 以 i 为下标，表示 nums[i, n - 1] 的元素和
-        for(int i = 0; i < n; i++){
-            //TODO nick 20250718 回家写
+        arr1[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            arr1[i] = nums[i] + arr1[i + 1];
         }
+        System.out.println("arr1: " + Arrays.toString(arr1));
 
         int[] arr2 = new int[n]; // 以 i 为下标，表示 nums[0, i] 的以 0 为左边界的子数组的最大和
+        int[] tmpArr2 = new int[n]; // nums[0, i] 的元素和
+        arr2[0] = nums[0];
+        tmpArr2[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            tmpArr2[i] = tmpArr2[i - 1] + nums[i];
+            // [0, i] 的最大前缀和 = [0, i - 1] 最大前缀和 或 [0, i-1]的和 + 当前位
+            arr2[i] = Math.max(arr2[i - 1], tmpArr2[i - 1] + nums[i]);
+        }
+        System.out.println("arr2: " + Arrays.toString(arr2));
+
+        // [0, n-1] 的最大子数组和
+        int max = arr2[0];
+        for(int arr2Tmp : arr2){
+            max = Math.max(max, arr2Tmp);
+        }
+        System.out.println("max of [0, n - 1]: " + max);
+
+        int[] arr3 = new int[n];
+        arr3[0] = max;
+        for(int i = 1; i <= n - 1 ; i++){
+            // [i,n-1] 的元素和 + [0, i-1]以 0  为左边界的子数组最大和
+            // = 跨右边界的子数组的最大和 [i, n - 1] & [0, i - 1]
+            arr3[i] = arr1[i] + arr2[i - 1];
+        }
+        System.out.println("arr3: " + Arrays.toString(arr3));
+
+        // 找到最大值
         for(int i = 0; i < n; i++){
-
+            max = Math.max(max, arr3[i]);
         }
-        int pre = nums[0];
-        int max = nums[0];
-        for (int i = 0; i < n; i++) {
-            if (i != 0) {
-                pre = Math.max(nums[i], nums[i] + pre);
-                max = Math.max(max, pre);
-            }
+        System.out.println("final max: " + max);
 
-
-        }
-
+        return max;
     }
 
 
---
+++ src/test/java/org/nick/learn/leetcode/p918/P918Solution4Test.java
@@ -0,0 +1,21 @@
+package org.nick.learn.leetcode.p918;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P918Solution4Test {
+    P918Solution4 solution = new P918Solution4();
+
+    @Test
+    void maxSubarraySumCircular() {
+        int max = solution.maxSubarraySumCircular(new int[]{5, -3, 5});
+        assertEquals(10, max);
+    }
+
+    @Test
+    void maxSubarraySumCircular2() {
+        int max = solution.maxSubarraySumCircular(new int[]{-3, -2, -3});
+        assertEquals(-3, max);
+    }
+}
