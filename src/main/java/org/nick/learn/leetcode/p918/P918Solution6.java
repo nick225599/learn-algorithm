b005e94b032caaa48211fdc9a7ad6378fb61add4
nick225599 <nick225599@163.com>
2025-07-21 15:05:50 +0800

    leetcode 918. Maximum Sum Circular Subarray，降低系数

---
+++ src/main/java/org/nick/learn/leetcode/p918/P918Solution6.java
@@ -0,0 +1,36 @@
+package org.nick.learn.leetcode.p918;
+
+public class P918Solution6 {
+    // 作者：力扣官方题解
+    // 链接：https://leetcode.cn/problems/maximum-sum-circular-subarray/solutions/2350660/huan-xing-zi-shu-zu-de-zui-da-he-by-leet-elou/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+    // dp 法的官方题解
+    // 比自己改良的 Solution5 时间复杂度一样，但是空间复杂度更差，实际运行结果我的 Solution5 也快 1ms
+    public int maxSubarraySumCircular(int[] nums) {
+        int n = nums.length;
+        int[] leftMax = new int[n];
+        // 对坐标为 0 处的元素单独处理，避免考虑子数组为空的情况
+        leftMax[0] = nums[0];
+        int leftSum = nums[0];
+        int pre = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < n; i++) {
+            pre = Math.max(pre + nums[i], nums[i]);
+            res = Math.max(res, pre);
+            leftSum += nums[i];
+            leftMax[i] = Math.max(leftMax[i - 1], leftSum);
+        }
+
+        // 从右到左枚举后缀，固定后缀，选择最大前缀
+        int rightSum = 0;
+        for (int i = n - 1; i > 0; i--) {
+            rightSum += nums[i];
+            res = Math.max(res, rightSum + leftMax[i - 1]);
+        }
+        return res;
+    }
+
+
+}
