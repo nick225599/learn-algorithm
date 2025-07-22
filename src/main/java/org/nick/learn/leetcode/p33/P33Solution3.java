66cbfa400ccf02fd28784d2bbee87c446655527b
nick225599 <nick225599@163.com>
2025-07-22 17:01:06 +0800

    leetcode 33. Search in Rotated Sorted Array，官方题解

---
+++ src/main/java/org/nick/learn/leetcode/p33/P33Solution3.java
@@ -0,0 +1,43 @@
+package org.nick.learn.leetcode.p33;
+
+public class P33Solution3 {
+
+//    作者：力扣官方题解
+//    链接：https://leetcode.cn/problems/search-in-rotated-sorted-array/solutions/220083/sou-suo-xuan-zhuan-pai-xu-shu-zu-by-leetcode-solut/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+    // 相比于自己的二分，少一部分边界修正逻辑，逻辑更清晰，更不容易出错
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+}
