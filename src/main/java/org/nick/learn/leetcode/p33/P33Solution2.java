9975ebae30c267f7fd68470067b9af398cc54291
nick225599 <nick225599@163.com>
2025-07-22 16:44:31 +0800

    leetcode 33. Search in Rotated Sorted Array，自己尝试二分

--- src/main/java/org/nick/learn/leetcode/p33/P33Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p33/P33Solution2.java
@@ -6,19 +6,23 @@ public class P33Solution2 {
     public int search(int[] nums, int target) {
         int n = nums.length;
         int l = 0;
-        int r = n - 1;
+        int r = n * 3 - 1;
         while (l <= r) {
             int m = l + ((r - l) >> 1);
-            int num = nums[m];
+            int num = nums[m % n];
             if (num < target) {
                 // l, m, target, r
                 // target 在 m 的右边 [m+1, r]
                 l = m + 1;
 
-                while (nums[l + ((r - l) >> 1)] < num) {
+                while (true) {
                     int newM = l + ((r - l) >> 1);
-                    int newNum = nums[newM];
-                    r = l + ((r - l) >> 1);
+                    int newNum = nums[newM % n];
+                    if (newNum < num) {
+                        r = l + ((r - l) >> 1);
+                    } else {
+                        break;
+                    }
                 }
 
                 // 试算一下，如果新 [m+1, r] 的中间节点值m2 比 num 小，说明新 m 跨界了
@@ -28,15 +32,18 @@ public class P33Solution2 {
                 // target 在 m 的左边
                 r = m - 1;
 
-                while (nums[l + ((r - l) >> 1)] < num) {
+                while (true) {
                     int newM = l + ((r - l) >> 1);
-                    int newNum = nums[newM];
-                    l = l + ((r - l) >> 1);
+                    int newNum = nums[newM % n];
+                    if (newNum > num) {
+                        l = l + ((r - l) >> 1);
+                    } else {
+                        break;
+                    }
                 }
 
-
             } else {
-                return m;
+                return m % n;
             }
         }
         return -1;
--- src/test/java/org/nick/learn/leetcode/p33/P33Solution2Test.java
+++ src/test/java/org/nick/learn/leetcode/p33/P33Solution2Test.java
@@ -2,13 +2,23 @@ package org.nick.learn.leetcode.p33;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class P33Solution2Test {
     P33Solution2 solution = new P33Solution2();
 
-
     @Test
     void search() {
+        assertEquals(4, solution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+
+    @Test
+    void search2() {
+        assertEquals(-1, solution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+    }
+
+    @Test
+    void search3() {
+        assertEquals(0, solution.search(new int[]{1}, 1));
     }
 }
