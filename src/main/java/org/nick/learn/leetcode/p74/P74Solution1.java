d790fbf8b9e372b5b7abb317e11a85132ac1a887
nick225599 <nick225599@163.com>
2025-07-22 13:45:37 +0800

    leetcode 74. Search a 2D Matrix，0ms 击败100%

--- src/main/java/org/nick/learn/leetcode/p74/P74Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p74/P74Solution1.java
@@ -1,54 +1,30 @@
 package org.nick.learn.leetcode.p74;
 
-public class P74Solution1 {
-    public boolean searchMatrix(int[][] matrix, int target) {
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class P74Solution1 {
+
+    // 0ms 击败100%
+    public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
-        if (target < matrix[0][0] || target > matrix[n - 1][m - 1]) {
-            return false;
-        }
 
-        // 1. 先在行里找 target 在哪一行
-        int i = (n - 1) >> 1;
-        while (true) {
-            if (matrix[i][0] < target) {
-                if (matrix[i + 1][0] > target) {
-                    // target 就在第 i 行
-                    break;
-                } else {
-                    // target 在 [i, n - 1] 行
-                    i = ((n - 1 - i) >> 1) + i;
-                }
-            } else {
-                if (matrix[i][0] == target) {
-                    // target 就在第 i 行
-                    return true;
-                } else {
-                    // target 在 [0, i - 1] 行
-                    i = (i - 1) >> 1;
-                }
-            }
-        }
-
-        System.out.println(target + " 在 m[" + i + "][] 中");
-
-
-        // 2. 再在那一行的列里找 target 有没有
-        int j = (m - 1) >> 1;
-        while (true) {
-            System.out.println("j: " + j);
-            if (matrix[i][j] < target) {
-                // target 在 [j + 1, m] 列
-                j = ((j + 1 - m) >> 1) + (j + 1);
-            } else if (matrix[i][j] > target) {
-                // target 在 [0, j - 1] 列
-                j = (j - 1) >> 1;
+        // 拉平整个二维数组进行二分查找
+        int left = 0;
+        int right = n * m - 1;
+        while (left < right) {
+            int mid = ((right - left) >> 1) + left;
+            int num = matrix[mid / m][mid % m];
+            if (target < num) {
+                right = mid - 1;
+            } else if (target > num) {
+                left = mid + 1;
             } else {
                 return true;
             }
         }
-
-        //TODO nick 下午看下怎么退出死循环
+        return matrix[left / m][left % m] == target;
     }
+
 }
--- src/test/java/org/nick/learn/leetcode/p74/P74Solution1Test.java
+++ src/test/java/org/nick/learn/leetcode/p74/P74Solution1Test.java
@@ -2,6 +2,7 @@ package org.nick.learn.leetcode.p74;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class P74Solution1Test {
@@ -19,12 +20,17 @@ class P74Solution1Test {
 
     @Test
     void searchMatrix2() {
-        assertTrue(solution.searchMatrix(new int[][]{
-                // [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+        assertFalse(solution.searchMatrix(new int[][]{
                 {1, 3, 5, 7},
                 {10, 11, 16, 20},
                 {23, 30, 34, 60}
         }, 13));
     }
 
+    @Test
+    void testSearchMatrix() {
+        assertFalse(solution.searchMatrix(new int[][]{
+                {1, 3}
+        }, 2));
+    }
 }
