8b0720b7926841f3ba94d18e260fa709278c3173
nick225599 <nick225599@163.com>
2025-07-23 10:14:47 +0800

    leetcode 34. Find First and Last Position of Element in Sorted Array，逻辑太混乱了，搞不定

--- src/main/java/org/nick/learn/leetcode/p34/P34Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p34/P34Solution1.java
@@ -1,5 +1,8 @@
 package org.nick.learn.leetcode.p34;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class P34Solution1 {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
@@ -10,21 +13,54 @@ public class P34Solution1 {
             return nums[0] == target ? new int[]{0, 0} : new int[]{-1, -1};
         }
 
-        // 1. 找到 < target 的最大值的右下标
-        //TODO nick 20250722 咋找？？？
+        // 1. 找到小于 target 的最大值的右下标
         int l = 0;
         int r = n - 1;
-        int t = nums[0];
         while (l <= r) {
-            int m = (r + l) / 2;
-            if (target < nums[m]) {
-                r = m - 1;
+            int midIndex = (r + l) / 2;
+            int midNum = nums[midIndex];
+            if (target < midNum) {
+                r = midIndex - 1;
+            } else if (midNum < target) {
+                if (l == midIndex && l + 1 == r) {
+                    break;
+                }
+                l = midIndex;
             } else {
-                l = m + 1;
+                r = midIndex - 1;
             }
         }
+        int left = r;
+        if (left < 0) {
+            return new int[]{-1, -1};
+        }
 
-        // 2. 找到 > target 的最小值的左下标
+        // 2. 找到大于 target 的最小值的左下标
+        l = 0;
+        r = n - 1;
+        while (l <= r) {
+            int midIndex = (l + r) / 2;
+            int midNum = nums[midIndex];
+            if (target < midNum) {
+                if (r == midIndex && l + 1 == r) {
+                    break;
+                }
+                r = midIndex;
+            } else if (midNum < target) {
+                l = midIndex + 1;
+            } else {
+                l = midIndex + 1;
+            }
+        }
+        int right = l;
+        if (right > n - 1) {
+            return new int[]{-1, -1};
+        }
 
+        if (left + 1 == right) {
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{left + 1, right - 1};
+        }
     }
 }
---
+++ src/test/java/org/nick/learn/leetcode/p34/P34Solution1Test.java
@@ -0,0 +1,35 @@
+package org.nick.learn.leetcode.p34;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P34Solution1Test {
+    P34Solution1 solution = new P34Solution1();
+
+    @Test
+    void searchRange() {
+        int[] arr = solution.searchRange(new int[]{5,7,7,8,8,10}, 8);
+        assertEquals("[3, 4]", Arrays.toString(arr));
+    }
+
+    @Test
+    void searchRange2() {
+        int[] arr = solution.searchRange(new int[]{5,7,7,8,8,10}, 6);
+        assertEquals("[-1, -1]", Arrays.toString(arr));
+    }
+
+    @Test
+    void searchRange3() {
+        int[] arr = solution.searchRange(new int[]{5,7,7,9,9,10}, 3);
+        assertEquals("[-1, -1]", Arrays.toString(arr));
+    }
+
+    @Test
+    void searchRange4() {
+        int[] arr = solution.searchRange(new int[]{5,7,7,9,9,10}, 11);
+        assertEquals("[-1, -1]", Arrays.toString(arr));
+    }
+}
