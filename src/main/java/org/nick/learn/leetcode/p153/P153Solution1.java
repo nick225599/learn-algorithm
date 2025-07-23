ebf3ee152f0f42a6e0a623eee3a7998af6f91f3b
nick225599 <nick225599@163.com>
2025-07-23 15:48:06 +0800

    leetcode 153. Find Minimum in Rotated Sorted Array，自己尝试完成

--- src/main/java/org/nick/learn/leetcode/p153/P153Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p153/P153Solution1.java
@@ -10,7 +10,9 @@ public class P153Solution1 {
         int r = n - 1;
         while (l < r) {
             int m = l + ((r - l) >> 1);
-            if (nums[l] <= nums[m] && nums[l] > nums[r]) {
+            if (nums[l] <= nums[m] // [l, m] 是递增的
+                    && nums[l] > nums[r] // 且左半区间的右边界小于左半边的左边界，那么最小值在右半边界
+            ) {
                 l = m + 1;
             } else {
                 r = m;
