9308268f3c3db6d6d9dbd8f3f9267f23a40368dc
nick225599 <nick225599@163.com>
2025-07-23 17:43:56 +0800

    leetcode 215. Kth Largest Element in an Array，阅读了题目

---
+++ src/main/java/org/nick/learn/leetcode/p215/P215Solution1.java
@@ -0,0 +1,13 @@
+package org.nick.learn.leetcode.p215;
+
+public class P215Solution1 {
+
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+
+        //TODO 感觉快排可以在 O(nlogn)内完成查找
+        //TODO 建立大顶堆也行
+
+        return -1;
+    }
+}
