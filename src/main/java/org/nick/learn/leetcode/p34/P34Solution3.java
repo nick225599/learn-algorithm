e296919848792f43123dc5a57847e79bc81be087
nick225599 <nick225599@163.com>
2025-07-23 14:39:31 +0800

    leetcode 34. Find First and Last Position of Element in Sorted Array，受网友启发

--- src/main/java/org/nick/learn/leetcode/p34/P34Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p34/P34Solution3.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class P34Solution3 {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
+
         if (n == 0) {
             return new int[]{-1, -1};
         }
@@ -13,27 +14,27 @@ public class P34Solution3 {
             return nums[0] == target ? new int[]{0, 0} : new int[]{-1, -1};
         }
 
-        int l = 0;
-        int r = n - 1;
-        int left = -1;
-        while (l <= r) {
-            int m = l + ((r - l) >> 1);
-            if (target < nums[m]) {
-                r = m - 1;
-            } else if (target > nums[m]) {
-                l = m + 1;
-            } else {
-                r = m - 1;
-                left = m;
-            }
-        }
-        if (left == -1) {
+        // 查找左边界
+        int startingPosition = binarySearch(nums, n, target, true);
+
+        if (startingPosition == -1) {
             return new int[]{-1, -1};
         }
 
-        l = 0;
-        r = n - 1;
-        int right = -1;
+        // 查找右边界
+        int endingPosition = binarySearch(nums, n, target, false);
+
+        return new int[]{startingPosition, endingPosition};
+    }
+
+    /**
+     * @param searchStartingPosition true: search the starting position of a given target value;
+     *                               false: search the ending position of a given target value
+     */
+    private int binarySearch(int[] nums, int n, int target, boolean searchStartingPosition) {
+        int l = 0;
+        int r = n - 1;
+        int position = -1;
         while (l <= r) {
             int m = l + ((r - l) >> 1);
             if (target < nums[m]) {
@@ -41,10 +42,14 @@ public class P34Solution3 {
             } else if (target > nums[m]) {
                 l = m + 1;
             } else {
-                l = m + 1;
-                right = m;
+                if (searchStartingPosition) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+                position = m;
             }
         }
-        return new int[]{left, right};
+        return position;
     }
 }
---
+++ src/main/java/org/nick/learn/leetcode/p34/P34Solution4.java
@@ -0,0 +1,38 @@
+package org.nick.learn.leetcode.p34;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class P34Solution4 {
+    //    作者：力扣官方题解
+    //    链接：https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/solutions/504484/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-de-di-3-4/
+    //    来源：力扣（LeetCode）
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * @param lower 如果 lower 为 true，则查找第一个大于等于 target 的下标，否则查找第一个大于 target 的下标
+     */
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
+}
