5fbf27ce2e99f26efb78a2eed9ce76637798551c
nick sun <nick.evans@outlook.com>
2025-07-24 14:37:03 +0800

    leetcode 215. Kth Largest Element in an Array，单轴快排

--- src/main/java/org/nick/learn/leetcode/p215/P215Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p215/P215Solution2.java
@@ -17,19 +17,26 @@ public class P215Solution2 {
         if (l == r) return nums[k];
         int x = nums[l], i = l - 1, j = r + 1;
         while (i < j) {
-            do i++; while (nums[i] < x);
-            do j--; while (nums[j] > x);
+            do {
+                i++;
+            } while (nums[i] < x);
+
+            do {
+                j--;
+            } while (nums[j] > x);
+
             if (i < j) {
                 int tmp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = tmp;
             }
         }
-        if (k <= j) return quickselect(nums, l, j, k);
-        else return quickselect(nums, j + 1, r, k);
+        if (k <= j) {
+            return quickselect(nums, l, j, k);
+        } else {
+            return quickselect(nums, j + 1, r, k);
+        }
     }
-
-
 
 
 }
