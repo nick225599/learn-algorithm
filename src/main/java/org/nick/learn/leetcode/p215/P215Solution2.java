367a24d4676891b4df8360217c1a47d1c25869cc
nick sun <nick.evans@outlook.com>
2025-07-24 14:45:26 +0800

    leetcode 215. Kth Largest Element in an Array，双指针单轴快排

--- src/main/java/org/nick/learn/leetcode/p215/P215Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p215/P215Solution2.java
@@ -7,12 +7,12 @@ public class P215Solution2 {
     // 来源：力扣（LeetCode）
     // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
-    // 双轴快排
     public int findKthLargest(int[] nums, int k) {
         int n = nums.length;
         return quickselect(nums, 0, n - 1, n - k);
     }
 
+    // 双指针快排
     int quickselect(int[] nums, int l, int r, int k) {
         if (l == r) return nums[k];
         int x = nums[l], i = l - 1, j = r + 1;
