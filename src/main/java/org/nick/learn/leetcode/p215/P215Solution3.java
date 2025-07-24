04dc6b2e8633109c360ca616eaee1b5b28c925ba
nick sun <nick.evans@outlook.com>
2025-07-24 14:25:24 +0800

    leetcode 215. Kth Largest Element in an Array，堆排序

--- src/main/java/org/nick/learn/leetcode/p215/P215Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p215/P215Solution3.java
@@ -1,13 +1,22 @@
 package org.nick.learn.leetcode.p215;
 
+import java.util.PriorityQueue;
+
 public class P215Solution3 {
 
-    int ans = -1;
-
-    // 徒手写堆
+    // 80ms 击败 16%
     public int findKthLargest(int[] nums, int k) {
         int n = nums.length;
-
-        return -1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(n);
+        for(int num : nums){
+            queue.add(num);
+        }
+        int i = 0;
+        int ans = 0;
+        while(i != n - k + 1){
+            ans = queue.poll();
+            i++;
+        }
+        return ans;
     }
 }
