b32af7965dc70dfb6b5ff8f4c121d7a2b834c3c8
nick225599 <nick225599@163.com>
2025-07-25 10:29:12 +0800

    leetcode 912. Sort an Array，单轴排序写错了

--- src/main/java/org/nick/learn/leetcode/p912/P912Solution7.java
+++ src/main/java/org/nick/learn/leetcode/p912/P912Solution7.java
@@ -1,8 +1,8 @@
 package org.nick.learn.leetcode.p912;
 
-public class P912Solution6 {
+public class P912Solution7 {
 
-    // 快排 Lomuto 分区法，选最左侧元素作为 pivot
+    // 快排 Lomuto 分区法，选最右侧元素作为 pivot
     public int[] sortArray(int[] nums) {
         this.quickSort(nums, 0, nums.length - 1);
         return nums;
@@ -16,12 +16,11 @@ public class P912Solution6 {
         }
     }
 
-    // TODO 以前这个错在哪？
-    private int lomutoParitition2(int[] nums, int l, int r) {
-        int pivot = nums[l];
+    private int lomutoParitition(int[] nums, int l, int r) {
+        int pivot = nums[r];
         int j = l - 1;
-        for (int i = l + 1; i <= r; i++) {
-            if (nums[i] < pivot && i != j + 1) {
+        for (int i = l; i < r; i++) {
+            if (nums[i] < pivot) {
                 j++;
                 this.swap(nums, i, j);
             }
@@ -32,24 +31,6 @@ public class P912Solution6 {
         } else {
             return j + 1;
         }
-    }
-
-    // 写对了
-    private int lomutoParitition(int[] nums, int l, int r) {
-        int pivot = nums[l];
-        int j = l + 1;
-        for (int i = l + 1; i <= r; i++) {
-            if (nums[i] < pivot) {
-                if (i != j) {
-                    this.swap(nums, i, j);
-                }
-                j++;
-            }
-        }
-        if (l != j - 1) {
-            this.swap(nums, l, j - 1);
-        }
-        return j - 1;
     }
 
     private void swap(int[] nums, int i, int j) {
