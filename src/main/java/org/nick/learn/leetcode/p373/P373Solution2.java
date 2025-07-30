8823e3a5a6970d4ff600fca19e4683324753fd8a
nick225599 <nick225599@163.com>
2025-07-30 17:00:18 +0800

    leetcode 373. Find K Pairs with Smallest Sums，自研解法，debug 试试

--- src/main/java/org/nick/learn/leetcode/p373/P373Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p373/P373Solution2.java
@@ -29,7 +29,11 @@ public class P373Solution2 {
                 r1 = arr[0];
                 r2 = arr[1];
             }
-            if (r1 == -1 || (nums1[l1] + nums2[l2] < nums1[r1] + nums2[r2])) {
+            if (l1 == -1 && l2 == -1 && r1 == -1 && r2 == -1) {
+                throw new IllegalStateException();
+            } else if ((r1 == -1 && r2 == -1)
+                    || (l1 != -1 && l2 != -1 && nums1[l1] + nums2[l2] < nums1[r1] + nums2[r2])) {
+                // 使用左轴固定值
                 pair = new ArrayList<>(2);
                 pair.add(nums1[l1]);
                 pair.add(nums2[l2]);
@@ -37,7 +41,9 @@ public class P373Solution2 {
                 k--;
                 leftUsed = true;
                 rightUsed = false;
-            } else if (l1 == -1 || (nums1[l1] + nums2[l2] > nums1[r1] + nums2[r2])) {
+            } else if ((l1 == -1 && l2 == -1)
+                    || ((r1 != -1 && r2 != -1) && nums1[l1] + nums2[l2] > nums1[r1] + nums2[r2])) {
+                // 使用右轴固定值
                 pair = new ArrayList<>(2);
                 pair.add(nums1[r1]);
                 pair.add(nums2[r2]);
@@ -46,6 +52,7 @@ public class P373Solution2 {
                 leftUsed = false;
                 rightUsed = true;
             } else {
+                // 同时使用左右轴值
                 pair = new ArrayList<>(2);
                 pair.add(nums1[l1]);
                 pair.add(nums2[l2]);
@@ -64,29 +71,33 @@ public class P373Solution2 {
 
     private int[] moveToNext(int n1, int n2, int l1, int l2, int r1, int r2, boolean isLeft) {
         if (isLeft) {
-            while (l1 != -1 && (l1 <= r1 && l2 <= r2)) {
-                l2++;
-                if (l2 == n2) {
-                    l1++;
-                    l2 = 0;
-                    if (l1 == n1) {
-                        l1 = l2 = -1;
-
+            while (l1 != -1) {
+                do {
+                    l2++;
+                    if (l2 == n2) {
+                        l1++;
+                        l2 = 0;
+                        if (l1 == n1) {
+                            l1 = l2 = -1;
+                            break;
+                        }
                     }
-                }
+                } while (l1 <= r1 && l2 <= r2);
             }
             return new int[]{l1, l2};
         } else {
-            while (r2 != -1 && (r1 <= l1 && r2 <= l2)) {
-                r1++;
-                if (r1 == n1) {
-                    r1 = 0;
-                    r2++;
-                    if (r2 == n2) {
-                        r1 = -1;
-                        r2 = -1;
+            while (r2 != -1) {
+                do {
+                    r1++;
+                    if (r1 == n1) {
+                        r1 = 0;
+                        r2++;
+                        if (r2 == n2) {
+                            r1 = r2 = -1;
+                            break;
+                        }
                     }
-                }
+                } while (r1 <= l1 && r2 <= l2);
             }
             return new int[]{r1, r2};
         }
