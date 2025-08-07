e4e3bddf7106bbefb9d0d0516a5645f9faae4b59
nick sun <nick.evans@outlook.com>
2025-08-07 15:03:39 +0800

    leetcode 295. Find Median from Data Streamm，有序列表 + 双指针法

--- src/main/java/org/nick/learn/leetcode/p295/P295Solution6.java
+++ src/main/java/org/nick/learn/leetcode/p295/P295Solution6.java
@@ -5,6 +5,9 @@ import java.util.TreeMap;
 public class P295Solution6 {
 
     // 仿制官方题解二
+    // 双堆法 或 有序集合+双指针法，空间复杂度都是 O(n)，
+    // 但是如果数据基本不重复，则有序集合法存在一定的空间利用率不足。
+    // 但如果数据存在大量重复元素，则有序集合空间利用率能够大幅降低，达到类似计数排序的效果
     static class MedianFinder {
 
         TreeMap<Integer, Integer> nums;
@@ -22,6 +25,9 @@ public class P295Solution6 {
             right = new int[2];
         }
 
+        // 时间复杂度：
+        // 双堆法 O(logn)
+        // 有序列表+双指针 O(logn) 耗在 hash map 的 get 和 put 方法里
         public void addNum(int num) {
             nums.put(num, nums.getOrDefault(num, 0) + 1);
             if (n == 0) {
@@ -47,6 +53,8 @@ public class P295Solution6 {
             n++;
         }
 
+        // 时间复杂度
+        // 双堆法和有序列表双指针法都是 O(1)
         public double findMedian() {
             return (left[0] + right[0]) / 2.0;
         }
