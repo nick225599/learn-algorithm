9d010528c9aabbd81aecb34f7c03fd9fcc2d6471
nick sun <nick.evans@outlook.com>
2025-08-07 16:29:51 +0800

    leetcode 68. Text Justification，自己尝试

--- src/main/java/org/nick/learn/leetcode/p68/P68Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p68/P68Solution1.java
@@ -1,49 +1,33 @@
 package org.nick.learn.leetcode.p68;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class P68Solution1 {
     public List<String> fullJustify(String[] words, int maxWidth) {
+        int n = words.length;
         List<String> lines = new ArrayList<>();
-        int usedIndex = -1;
-        while (usedIndex < words.length - 1) {
-            // 判断一行能用第几到第几个 words
-            int startIndex = usedIndex + 1;
-            int curIndex = startIndex;
-            int endIndex;
-            int width = 0;
-            while (width <= maxWidth && curIndex < words.length) {
-                if (width != 0) {
-                    width += 1;
+        int startIndex = -1, endIndex = -1;
+        int lineNumber = 1;
+        while (endIndex != words.length - 1) {
+            startIndex = endIndex + 1;
+            // 判断这行能用第几到第几个单词，一共几个单词，几个单词间隙
+            int curIdx;
+            int lineWidth = 0;
+            for (curIdx = startIndex; lineWidth <= maxWidth && curIdx < n; curIdx++) {
+                if(lineWidth != 0){
+                    lineWidth += 1;
                 }
-                width += words[curIndex].length();
-                curIndex++;
+                String word = words[curIdx];
+                int wordLength = word.length();
+                lineWidth += wordLength;
+                log.info("cur word: {}, word length: {}, line width: {}", word, wordLength, lineWidth);
             }
-            width -= words[curIndex - 1].length();
-            endIndex = curIndex - 2;
-            usedIndex = endIndex;
-
-            int left = maxWidth - width;
-            int n = endIndex - startIndex + 1;
-            int everySpaceNumber = (int) Math.ceil((double) left / n);
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = startIndex; i <= endIndex; i++) {
-                if (sb.length() != 0) {
-                    sb.append(" ");
-                }
-                if (left > everySpaceNumber) {
-                    sb.append(" ".repeat(everySpaceNumber));
-                    left -= everySpaceNumber;
-                } else if (left > 0) {
-                    sb.append(" ".repeat(left));
-                    left = 0;
-                }
-                sb.append(words[0]);
-            }
-            lines.add(sb.toString());
-            System.out.println("used index: " + usedIndex);
+            endIndex = curIdx - 1;
+            log.info("第 {} 行，开始单词 {}, 结束单词 {}", lineNumber, words[startIndex], words[endIndex]);
         }
 
         return lines;
