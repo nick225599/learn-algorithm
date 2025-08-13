23222e463b4514c28f0c46146504ee01f236881f
nick sun <nick.evans@outlook.com>
2025-08-13 22:57:22 +0800

    leetcode 68. Text Justification，官方题解

---
+++ src/main/java/org/nick/learn/leetcode/p68/P68Solution3.java
@@ -0,0 +1,24 @@
+package org.nick.learn.leetcode.p68;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class P68Solution3 {
+
+    /*
+    算法能力的核心是 “解决问题的能力”，而不仅仅是 “掌握经典算法”。
+    力扣第 68 题虽然没有复杂的算法模型，但它通过贴近实际的文本排版场景，
+    考察了问题拆解、逻辑建模、边界处理、代码结构化等核心工程能力 —— 这些能力是写出可靠、
+    可维护代码的基础，也是算法工程师必备的素质。
+
+    在实际开发中，“把简单逻辑写对” 往往比 “实现复杂算法” 更常见，也更能体现编程功底。这正是这类题的价值所在。
+     */
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        //todo nick 20250813 沉住气 把这道题 好好写出来
+        return null;
+    }
+
+
+}
