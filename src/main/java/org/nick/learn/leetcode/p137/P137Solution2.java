d465257a34290b9d2c5afa4698fb90d2b1b39fae
nick sun <nick.evans@outlook.com>
2025-08-19 16:16:48 +0800

    leetcode 137. Single Number II，逐位运算

--- src/main/java/org/nick/learn/leetcode/p137/P137Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p137/P137Solution2.java
@@ -2,6 +2,7 @@ package org.nick.learn.leetcode.p137;
 
 import org.apache.commons.lang3.StringUtils;
 
+//@Slf4j
 public class P137Solution2 {
 
     // You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -9,28 +10,43 @@ public class P137Solution2 {
 
     // 不，傻瓜，还能按位求和
     // 没写对
+
+    // 4ms 击败 25.67 %
     public int singleNumber(int[] nums) {
         int ans = 0;
         for (int i = 0; i < 32; i++) {
             int bitSum = 0;
             for (int num : nums) {
                 // num 第 i 位是 0 还是 1 ？
-                int bitNum = ((1 << i) & num) == 1 ? 1 : 0;
+                int bitNum = ((num >> i) & 1) == 1 ? 1 : 0;
                 bitSum += bitNum;
 
-                String binaryString = StringUtils.leftPad(Integer.toBinaryString(num), 32, '0');
-
-
+//                log.info("num: {}, 二进制格式: {}, 第 {} 位的值为 {}, 第 {} 位的累加结果: {}",
+//                        num, this.intToBinaryStr(num), i, bitNum, i, bitSum );
             }
             // ans 第 i 位的值
             int bitVal = bitSum % 3;
-            System.out.println("第 " + i + " 位的值：" + bitVal);
-
+//            log.info("第 {} 位的累加结果: {},  ans 的第 {} 位为: {}",
+//                    i, bitSum, i, bitVal);
             // 组装 ans
             if (bitVal == 1) {
                 ans += 1 << i;
             }
+//            log.info("第 {} 位的计算结束后，ans 为：{}", i, ans);
         }
         return ans;
     }
+
+    private String intToBinaryStr(int num) {
+        String str = StringUtils.leftPad(Integer.toBinaryString(num), 32, '0');
+        String sb = str.substring(0, 8) + "_" +
+                str.substring(9, 16) + "_" +
+                str.substring(17, 24) + "_" +
+                str.substring(25, 32);
+        return "0b" + sb;
+
+    }
+
+
+
 }
