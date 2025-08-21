286ebff7bccbbb9cd47481cfe934b3013827352c
nick sun <nick.evans@outlook.com>
2025-08-21 09:48:00 +0800

    leetcode 137. Single Number II，数字电路

--- src/main/java/org/nick/learn/leetcode/p137/P137Solution5.java
+++ src/main/java/org/nick/learn/leetcode/p137/P137Solution5.java
@@ -2,7 +2,6 @@ package org.nick.learn.leetcode.p137;
 
 public class P137Solution5 {
 
-    //todo nick 20250819 待理解怎么用真值表设计出数字电路 https://www.doubao.com/thread/w233ee5acf878bd1c
     public int singleNumber(int[] nums) {
         int a = 0, b = 0;
         for (int num : nums) {
