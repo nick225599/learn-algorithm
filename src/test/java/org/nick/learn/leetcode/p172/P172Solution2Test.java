d5685d520d5a95a04c91d12aac5799b9df5c1df0
nick sun <nick.evans@outlook.com>
2025-08-28 11:08:11 +0800

    leetcode 172. Factorial Trailing Zeroes，logn 解法

---
+++ src/test/java/org/nick/learn/leetcode/p172/P172Solution2Test.java
@@ -0,0 +1,15 @@
+package org.nick.learn.leetcode.p172;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class P172Solution2Test {
+    P172Solution2 solution = new P172Solution2();
+
+    @Test
+    void trailingZeroes() {
+        assertEquals(7, solution.trailingZeroes(30));;
+
+    }
+}
