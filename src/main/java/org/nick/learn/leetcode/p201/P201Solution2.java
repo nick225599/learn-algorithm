6a9a4a800ab70ce86bb59a8eac5d45faa03e722a
nick225599 <nick225599@163.com>
2025-09-25 17:06:52 +0800

    leetcode 201. Bitwise AND of Numbers Range，完成 BK 法的学习。

--- src/main/java/org/nick/learn/leetcode/p201/P201Solution2.java
+++ src/main/java/org/nick/learn/leetcode/p201/P201Solution2.java
@@ -2,6 +2,8 @@ package org.nick.learn.leetcode.p201;
 
 public class P201Solution2 {
 
+    // 通过位移的方式求出公共前缀
+    // 时间复杂度 log(n) n 表示 int 型的位数
     public int rangeBitwiseAnd(int left, int right) {
         int shift = 0;
         while (left != right) {
--- src/main/java/org/nick/learn/leetcode/p201/P201Solution3.java
+++ src/main/java/org/nick/learn/leetcode/p201/P201Solution3.java
@@ -2,8 +2,15 @@ package org.nick.learn.leetcode.p201;
 
 public class P201Solution3 {
 
+    // Brian Kernighan 算法
+    // 时间复杂度：O(logn)。和位移方法类似，
+    // 算法的时间复杂度取决于 m 和 n 二进制展开的位数。尽管和位移方法具有相同的渐近复杂度，
+    // 但 Brian Kernighan 的算法需要的迭代次数会更少，因为它跳过了两个数字之间的所有零位。
     public int rangeBitwiseAnd(int left, int right) {
-       //TODO NICK 20250821 方法二：Brian Kernighan 算法
-        return -1;
+        int n = right;
+        while (left < n) {
+            n = n & n - 1;
+        }
+        return n;
     }
 }
