fcee0df19ffc3e28c1db8f92f74f7878e424f67b
nick225599 <nick225599@163.com>
2025-11-07 15:36:55 +0800

    leetcode 30. Substring with Concatenation of All Words，多线程朴素解法，没能通过第 159 号测试用例

--- src/main/java/org/nick/learn/leetcode/p30/P30Solution1.java
+++ src/main/java/org/nick/learn/leetcode/p30/P30Solution1.java
@@ -1,55 +1,96 @@
 package org.nick.learn.leetcode.p30;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
+@Slf4j
 public class P30Solution1 {
+    static class Solution {
 
-    List<String> allCombinations = new ArrayList<>();
+        public List<Integer> findSubstring(String s, String[] words) {
+            try {
+                int wLength = words[0].length();
+                int sLength = wLength * words.length;
+                if (s.length() < sLength) {
+                    return new ArrayList<>();
+                }
 
-    public List<Integer> findSubstring(String s, String[] words) {
-        // 使用回溯组合 words
-        //TODO nick 20250930 菜狗 甚至搞不定一个回溯写法
-        this.enumerateAllCombinations(words);
+                int endIndex = s.length() - sLength + 1;
+                List<Future<Integer>> futures = new ArrayList<>(endIndex);
+                List<Integer> result = new ArrayList<>();
+                try (ExecutorService executorService = Executors.newCachedThreadPool()) {
+                    for (int i = 0; i <= endIndex; i++) {
+                        int finalI = i;
+                        Future<Integer> future = executorService.submit(new Callable<Integer>() {
+                            @Override
+                            public Integer call() throws Exception {
+                                return callA(finalI, sLength, words, s);
+                            }
+                        });
+                        futures.add(future);
+                    }
+                    executorService.shutdown();
+                    executorService.awaitTermination(1, TimeUnit.MINUTES);
 
-        // 使用 indexOf 记录
-        return null;
-    }
-
-    // 给定一个字符串数组，怎么列举出所有的排列组合？
-    private List<String> enumerateAllCombinations(String[] words) {
-        int n = words.length;
-        List<String> combination = new ArrayList<>();
-        this.enumerateAllCombinations(words, combination);
-        return null;
-    }
-
-    private void enumerateAllCombinations(String[] words,List<String> combination) {
-
-        if (combination.size() == words.length) {
-            StringBuilder sb = new StringBuilder();
-            for(String word : combination){
-                sb.append(word);
+                    for (Future<Integer> future : futures) {
+                        Integer integer = future.get(100, TimeUnit.MICROSECONDS);
+                        if (null != integer) {
+                            result.add(integer);
+                        }
+                    }
+                    return result;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ArrayList<>();
             }
-            allCombinations.add(sb.toString());
-            return;
+
+            // 从 s 的第 0 个位置开始，一直到第 s.length() - words[0].length() * words.size()
+            // 判断每 word[0].length() 个字符，判断是否属于 words, 属于的话标记已使用
+
         }
 
-        for (String word : words) {
-            if (combination.contains(word)) {
-                continue;
+        private static Integer callA(final int finalI, int sLength, String[] words, String s) {
+            // 判断字符串 s 的 index: i, length: wLength 的子字符串，是否包含所有 words
+            int startIndex = finalI;
+            int endIndex = startIndex + sLength; // exclude
+            boolean[] used = new boolean[words.length];
+            while (startIndex < endIndex) {
+                boolean containAWord = Solution.containAWorld(s, startIndex, words, used);
+                if (containAWord) {
+                    startIndex += words[0].length();
+                } else {
+                    return null;
+                }
             }
+            return finalI;
+        }
 
-            // 不采用 i
-            this.enumerateAllCombinations(words, combination);
+        private static boolean containAWorld(String s, int index, String[] words, boolean[] used) {
+            for (int i = 0; i < words.length; i++) {
+                if (used[i]) {
+                    continue;
+                }
+                if (strEquals(s, index, words[i])) {
+                    used[i] = true;
+                    return true;
+                }
+            }
+            return false;
+        }
 
-            // 采用 i
-            combination.add(word);
-            this.enumerateAllCombinations(words, combination);
-            combination.remove(word);
-
-
+        private static boolean strEquals(String s, int index, String word) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) != s.charAt(index + i)) {
+                    return false;
+                }
+            }
+            return true;
         }
 
     }
+
 }
--- src/test/java/org/nick/learn/leetcode/p30/P30Solution1Test.java
+++ src/test/java/org/nick/learn/leetcode/p30/P30Solution1Test.java
@@ -1,17 +1,21 @@
 package org.nick.learn.leetcode.p30;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class P30Solution1Test {
     P30Solution1 solution = new P30Solution1();
+
     @Test
-    void findSubstring() {
+    void findSubstring() throws Exception {
+        String s = "barfoothefoobarman";
         String[] words = new String[]{"foo", "bar"};
-        solution.findSubstring("barfoothefoobarman", words);
+        List<Integer> list = solution.findSubstring(s, words);
+        log.info("res: {}", Arrays.toString(list.toArray()));
     }
+
 }
